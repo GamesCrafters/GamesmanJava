4ccb52a4931b7f7973c8a409777ace2b9fa950b5
sugis <sugis@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2009-01-29 09:26:05 +0000

    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@3753 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ src/edu/berkeley/gamesman/Gamesman.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package edu.berkeley.gamesman;
+
+/**
+ * @author steven
+ *
+ */
+public final class Gamesman {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
