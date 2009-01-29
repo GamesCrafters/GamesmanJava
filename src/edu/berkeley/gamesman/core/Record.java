47e2118549bea24f61691151a6d4f30eda232b6d
sugis <sugis@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2009-01-29 12:16:15 +0000

    ------------------------------------------------------------ revno: 66 committer: Steven Schlansker <stevenschlansker@gmail.com> branch nick: gamesman-java-trunk timestamp: Sat 2008-12-06 02:30:29 -0800 message:   A few fixes
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@3818 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/core/Record.java
+++ src/edu/berkeley/gamesman/core/Record.java
@@ -5,6 +5,9 @@ import java.io.DataOutput;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,7 +32,10 @@ public final class Record {
 		set(RecordFields.Value,primitiveValue.value);
 	}
 	
-	private Record(){}
+	private Record(Configuration conf2){
+		conf = conf2;
+		setupBits();
+	}
 
 	public void writeStream(DataOutput out) {
 		try {
@@ -42,9 +48,7 @@ public final class Record {
 	}
 
 	public static Record readStream(Configuration conf, DataInput in) {
-		Record r = new Record();
-		r.conf = conf;
-		r.setupBits();
+		Record r = new Record(conf);
 		r.readStream(in);
 		return r;
 	}
@@ -53,6 +57,8 @@ public final class Record {
 		try {
 			for (int i = 0; i < bits.length; i++) {
 				fields[i] = in.readInt();
+				System.out.println("Read field "+i+" to "+fields[i]);
+				if(fields[1] == 131072) Util.fatalError("WTF");
 			}
 		} catch (IOException e) {
 			Util.fatalError("Error while writing record: " + e);
@@ -64,9 +70,7 @@ public final class Record {
 	}
 
 	public static Record read(Configuration conf, ByteBuffer buf, long index) {
-		Record r = new Record();
-		r.conf = conf;
-		r.setupBits();
+		Record r = new Record(conf);
 		r.read(buf,index);
 		return r;
 	}
@@ -75,19 +79,21 @@ public final class Record {
 		
 	}
 
-	public int length() {
+	public final int length() {
 		return (maxbits + 7) / 8;
 	}
 	
-	public static int length(Configuration conf){
+	public static int length(final Configuration conf){
 		return new Record(conf,PrimitiveValue.Win).length();
 	}
 
-	public final void set(RecordFields field, int value) {
+	public final void set(final RecordFields field, final int value) {
+		System.out.println("Set "+field+" to "+value);
 		fields[sf.get(field).car] = value;
 	}
 
-	public final int get(RecordFields field) {
+	public final int get(final RecordFields field) {
+		System.out.println("Get "+field+" is "+fields[sf.get(field).car]);
 		return fields[sf.get(field).car];
 	}
 
@@ -105,9 +111,56 @@ public final class Record {
 		maxbits = bitOffset[bitOffset.length - 1] + bits[bits.length - 1];
 	}
 
-	public static Record combine(Configuration conf, List<Record> vals) {
-		Util.fatalError("TODO"); //TODO
-		return null;
+	public static Record combine(final Configuration conf, final List<Record> vals) {
+		Record rec = new Record(conf);
+		
+		final EnumMap<RecordFields,ArrayList<Integer>> map = new EnumMap<RecordFields, ArrayList<Integer>>(RecordFields.class);
+		for(RecordFields rf : conf.getStoredFields().keySet()){
+			map.put(rf, new ArrayList<Integer>(vals.size()));
+		}
+		for(Record r : vals){
+			for(RecordFields rf : conf.getStoredFields().keySet()){
+				map.get(rf).add(r.get(rf));
+			}
+		}
+		
+
+		loop: for(RecordFields rf : conf.getStoredFields().keySet()){
+			switch(rf){
+			case Value:
+				rec.set(rf,primitiveCombine(map.get(rf)).value);
+				break;
+			case Remoteness:
+				rec.set(rf, Collections.min(map.get(rf))+1);
+				break;
+			default:
+				Util.fatalError("Default case shouldn't have been reached");
+			}
+		}
+		
+		return rec;
+	}
+	
+	private static PrimitiveValue primitiveCombine(List<Integer> vals){
+		boolean seentie = false;
+		for(Integer iv : vals){
+			PrimitiveValue v = PrimitiveValue.values()[iv];
+			switch(v){
+			case Undecided:
+				return PrimitiveValue.Undecided;
+			case Lose:
+				return PrimitiveValue.Win;
+			case Tie:
+				seentie = true;
+				break;
+			case Win:
+				break;
+			default:
+				Util.fatalError("Illegal primitive value: "+iv);
+			}
+		}
+		if(seentie) return PrimitiveValue.Tie;
+		return PrimitiveValue.Lose;
 	}
 
 }
