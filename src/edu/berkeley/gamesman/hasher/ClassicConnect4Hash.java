360a7506860ed045b94a60c53e86574f39787bc7
sugis <sugis@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2009-01-29 11:20:11 +0000

    ------------------------------------------------------------ revno: 52 committer: Steven Schlansker <stevenschlansker@gmail.com> branch nick: gamesman-java timestamp: Tue 2008-11-18 15:35:09 -0800 message:    a
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@3804 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/hasher/ClassicConnect4Hash.java
+++ src/edu/berkeley/gamesman/hasher/ClassicConnect4Hash.java
@@ -2,9 +2,33 @@ package edu.berkeley.gamesman.hasher;
 
 import java.math.BigInteger;
 
-import edu.berkeley.gamesman.core.Hasher;
 import edu.berkeley.gamesman.core.TieredHasher;
+import edu.berkeley.gamesman.util.Pair;
 
 public class ClassicConnect4Hash extends TieredHasher<char[][]> {
+
+	@Override
+	public char[][] gameStateForTierIndex(int tier, BigInteger index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BigInteger numHashesForTier(int tier) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int numberOfTiers() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Pair<Integer, BigInteger> tierIndexForState(char[][] state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
