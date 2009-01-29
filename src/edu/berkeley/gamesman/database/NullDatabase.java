57881a34c8b17e964e7d7c7d6fc96be75681586c
sugis <sugis@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2009-01-29 10:31:31 +0000

    ------------------------------------------------------------ revno: 35 committer: Steven Schlansker <stevenschlansker@gmail.com> branch nick: gamesman-java timestamp: Sat 2008-11-15 15:31:17 -0800 message:   Added NullDatabase
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@3787 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ src/edu/berkeley/gamesman/database/NullDatabase.java
@@ -0,0 +1,28 @@
+package edu.berkeley.gamesman.database;
+
+import edu.berkeley.gamesman.core.Database;
+import edu.berkeley.gamesman.core.Values;
+import edu.berkeley.gamesman.util.Util;
+
+public final class NullDatabase extends Database {
+
+	@Override
+	public void close() {}
+
+	@Override
+	public void flush() {}
+
+	@Override
+	public DBValue getValue(Number loc) {
+		return Values.Win;
+	}
+
+	@Override
+	public void initialize(String url, DBValue exampleValue) {
+		Util.warn("Using NullDatabase, answers will be incorrect and nothing will be saved.");
+	}
+
+	@Override
+	public void setValue(Number loc, DBValue value) {}
+
+}
