f816c6d09bbdf2874c6ec3fe40231825b811df47
sugis <sugis@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2009-02-05 20:32:16 +0000

    Committed totally broken TopDownSolver
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@3856 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ src/edu/berkeley/gamesman/solver/TopDownSolver.java
@@ -0,0 +1,16 @@
+package edu.berkeley.gamesman.solver;
+
+import edu.berkeley.gamesman.core.Configuration;
+import edu.berkeley.gamesman.core.Game;
+import edu.berkeley.gamesman.core.Solver;
+import edu.berkeley.gamesman.core.WorkUnit;
+
+public class TopDownSolver extends Solver {
+
+	@Override
+	public WorkUnit prepareSolve(Configuration config, Game<?> game) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
