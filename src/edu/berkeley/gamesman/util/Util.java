a6873bd0a8c830c778ba344fc659b48daaa810c7
j-fly4 <j-fly4@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2009-03-05 11:01:11 +0000

    Removed unecessary import from Util.
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@4031 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/util/Util.java
+++ src/edu/berkeley/gamesman/util/Util.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.EnumSet;
