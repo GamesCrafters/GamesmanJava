3f022e36050e6b2d9aa9f1634a001b8576b90b23
j-fly4 <j-fly4@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2009-03-07 00:30:27 +0000

    Fallback to interactive console implemented.
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@4061 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/JythonInterface.java
+++ src/edu/berkeley/gamesman/JythonInterface.java
@@ -1,5 +1,10 @@
 package edu.berkeley.gamesman;
 
+import java.io.IOException;
+
+import jline.ConsoleReader;
+import jline.Terminal;
+
 import org.python.core.PyObject;
 import org.python.util.InteractiveConsole;
 import org.python.util.JLineConsole;
@@ -10,20 +15,24 @@ public final class JythonInterface {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//PythonInterpreter pi = new PythonInterpreter();
-
-		InteractiveConsole rc = new InteractiveConsole() {
-		};
+		InteractiveConsole rc = null;
 		try {
-			rc = new JLineConsole();
-		} catch(NoClassDefFoundError e) { //if we don't have the ReadlineLibrary
-			rc = new InteractiveConsole() {
-				@Override
-				public String raw_input(PyObject prompt) {
-					return super.raw_input(prompt);
-				}
-			};
+			ConsoleReader cr = new ConsoleReader();
+			System.out.print("Press enter to start gamesman-jython!\n");
+			if(cr.readLine() != null) //eclipse doesn't work with jline
+				rc = new JLineConsole();
+		} catch(Error e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		//if jline isn't working for some reason
+		rc = new InteractiveConsole() {
+			@Override
+			public String raw_input(PyObject prompt) {
+				return super.raw_input(prompt);
+			}
+		};
 		//this will let us put .py files in the junk directory, and things will just work =)
 		rc.exec(String.format("import sys; sys.path.append('%s/%s'); sys.path.append('%s/../%s');", System.getProperty("user.dir"), "junk", System.getProperty("user.dir"), "junk"));
 		rc.interact();
