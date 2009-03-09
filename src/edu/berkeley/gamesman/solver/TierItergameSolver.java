ddb9965942805fc906e195c086c637b82165fbc6
dspyz <dspyz@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2009-03-09 08:38:21 +0000

    Wasn't using TierItergameSolver in previous implementation
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@4128 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/solver/TierItergameSolver.java
+++ src/edu/berkeley/gamesman/solver/TierItergameSolver.java
@@ -3,27 +3,49 @@ package edu.berkeley.gamesman.solver;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 
+import edu.berkeley.gamesman.core.Configuration;
+import edu.berkeley.gamesman.core.Game;
 import edu.berkeley.gamesman.core.ItergameState;
 import edu.berkeley.gamesman.core.PrimitiveValue;
 import edu.berkeley.gamesman.core.Record;
+import edu.berkeley.gamesman.core.Solver;
 import edu.berkeley.gamesman.core.TieredIterGame;
+import edu.berkeley.gamesman.core.WorkUnit;
 import edu.berkeley.gamesman.util.DebugFacility;
 import edu.berkeley.gamesman.util.Pair;
+import edu.berkeley.gamesman.util.Task;
 import edu.berkeley.gamesman.util.Util;
 
 /**
- * TierSolver documentation stub
- * 
  * @author DNSpies
- * 
+ *
  */
-public final class TierItergameSolver extends TierSolver<ItergameState> {
+public class TierItergameSolver extends Solver {
+
+	protected TieredIterGame myGame;
+	protected Configuration conf;
+
+	@Override
+	public WorkUnit prepareSolve(Configuration inconf, Game<Object> game) {
+
+		myGame = Util.checkedCast(game);
+		tier = myGame.numberOfTiers() - 1;
+		offset = myGame.hashOffsetForTier(tier);
+		updater = new TierSolverUpdater();
+		conf = inconf;
+
+		return new TierSolverWorkUnit();
+	}
 
 	protected void solvePartialTier(TieredIterGame game, BigInteger start,
 			BigInteger end, TierSolverUpdater t) {
 		BigInteger current = start;
-		game.hashToState(start);
+		ItergameState s = game.hashToState(start);
+		game.setState(s);
 		while (current.compareTo(end) <= 0) {
 			current = current.add(BigInteger.ONE);
 			if (current.mod(BigInteger.valueOf(10000)).compareTo(
@@ -51,5 +73,116 @@ public final class TierItergameSolver extends TierSolver<ItergameState> {
 				game.nextHashInTier();
 		}
 		Util.debug(DebugFacility.SOLVER,"Reached end of partial tier at "+end);
+	}
+
+	protected int nextIndex = 0;
+	protected TierSolverUpdater updater;
+	protected BigInteger offset;
+	protected CyclicBarrier barr;
+	protected int tier;
+
+	private boolean needs2Sync;
+
+	protected Pair<BigInteger, BigInteger> nextSlice() {
+		if (needs2Sync) {
+			Util.debug(DebugFacility.THREADING,"Thread waiting to tier-sync");
+			try {
+				if (barr != null)
+					barr.await();
+			} catch (InterruptedException e) {
+				Util.fatalError(
+						"TierSolver thread was interrupted while waiting!", e);
+			} catch (BrokenBarrierException e) {
+				Thread.yield();
+				return nextSlice(); // Retry
+			}
+		}
+
+		synchronized (this) {
+			if (tier < 0)
+				return null;
+			final BigInteger step = BigInteger.valueOf(10000);
+			BigInteger ret = offset, end;
+			offset = offset.add(step);
+			end = ret.add(step);
+			if (end.compareTo(myGame.lastHashValueForTier(tier)) >= 0) {
+				end = myGame.lastHashValueForTier(tier);
+				tier--;
+				if (tier >= 0)
+					offset = myGame.hashOffsetForTier(tier);
+				needs2Sync = true;
+			}
+			return new Pair<BigInteger, BigInteger>(ret, end);
+		}
+	}
+
+	private final class TierSolverWorkUnit implements WorkUnit {
+
+		private int index;
+
+		TierSolverWorkUnit() {
+
+			// if(!(g instanceof TieredGame))
+			// Util.fatalError("Attempted to use tiered solver on non-tiered game");
+			this.index = nextIndex++;
+			barr = new CyclicBarrier(nextIndex);
+		}
+
+		@SuppressWarnings("synthetic-access")
+		public void conquer() {
+			Util.debug(DebugFacility.SOLVER,"Started the solver... (" , index , ")");
+			Thread.currentThread().setName(
+					"Solver (" + index + "): " + myGame.toString());
+
+			Pair<BigInteger, BigInteger> slice;
+			while ((slice = nextSlice()) != null) {
+				Util.debug(DebugFacility.THREADING,"Beginning to solve slice " , slice , " in thread "
+						, index);
+				solvePartialTier(myGame, slice.car, slice.cdr, updater);
+				db.flush();
+			}
+
+			synchronized (TierItergameSolver.this) {
+				if (barr != null) {
+					updater.complete();
+					barr.reset();
+					barr = null;
+				}
+			}
+		}
+
+		public List<WorkUnit> divide(int num) {
+			ArrayList<WorkUnit> arr = new ArrayList<WorkUnit>(num);
+			arr.add(this);
+			for (int i = 1; i < num; i++)
+				arr.add(new TierSolverWorkUnit());
+			return arr;
+		}
+	}
+
+	final class TierSolverUpdater {
+
+		private BigInteger total = BigInteger.ZERO;
+		private Task t;
+		private long lastUpdate = 0;
+
+		TierSolverUpdater() {
+			t = Task.beginTask("Tier solving \"" + myGame.toString() + "\"");
+			t.setTotal(myGame.lastHashValueForTier(myGame.numberOfTiers() - 1));
+		}
+
+		synchronized void calculated(int howMuch) {
+			total = total.add(BigInteger.valueOf(howMuch));
+			if (t != null && System.currentTimeMillis() - lastUpdate > 1000) {
+				t.setProgress(total);
+				lastUpdate = System.currentTimeMillis();
+			}
+		}
+
+		public synchronized void complete() {
+			if (t != null)
+				t.complete();
+			t = null;
+		}
 	}
 }
