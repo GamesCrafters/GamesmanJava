995a014c0783d7917e0494e057430d9c31e94102
jinsucraft <jinsucraft@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2009-03-13 20:33:51 +0000

    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@4183 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/shell/DatabaseModule.java
+++ src/edu/berkeley/gamesman/shell/DatabaseModule.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
 import java.math.BigInteger;
 import java.net.URI;
@@ -13,7 +14,10 @@ import java.util.ArrayList;
 import java.util.Properties;
 
 import edu.berkeley.gamesman.core.Configuration;
+import edu.berkeley.gamesman.core.Game;
+import edu.berkeley.gamesman.core.Hasher;
 import edu.berkeley.gamesman.core.Record;
+import edu.berkeley.gamesman.core.RecordFields;
 import edu.berkeley.gamesman.util.Util;
 import edu.berkeley.gamesman.core.Database;
 import edu.berkeley.gamesman.database.BlockDatabase;
@@ -28,6 +32,10 @@ public class DatabaseModule extends UIModule {
 		loc = BigInteger.ZERO;
 		requiredPropKeys = new ArrayList<String>();
 		requiredPropKeys.add("gamesman.db.uri");
+		requiredPropKeys.add("gamesman.game");
+		requiredPropKeys.add("gamesman.hasher");
+		requiredPropKeys.add("gamesman.solver");
+		requiredPropKeys.add("gamesman.database");
 		//what property would it want? 
 		//maybe that if conf has a database path?
 		
@@ -47,7 +55,7 @@ public class DatabaseModule extends UIModule {
 	/**
 	 * u_openDatabase calls openDatabase with curConf.
 	 */
-	protected void u_openDatabase(ArrayList<String> args) {
+	protected void u_openDatabase(ArrayList<String> args) {	
 		openDatabase();
 	}
 	
@@ -57,6 +65,9 @@ public class DatabaseModule extends UIModule {
 	 * @param conf configuration it's trying to read a database file from.
 	 */
 	private void openDatabase() {
+		proccessCommand("i");
+		db = conf.openDatabase();
+		/*
 		db = new BlockDatabase();
 		URI f;
 		try {
@@ -67,7 +78,7 @@ public class DatabaseModule extends UIModule {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		*/
 		/*
 		try {
 			f = new URI(conf.getProperty("gamesman.db.uri"));
@@ -92,17 +103,17 @@ public class DatabaseModule extends UIModule {
 	 * u_readRecord reads a record from theRecord into curRecord.
 	 */	
 	protected void u_readRecord(ArrayList<String> args) {
-		if(args.isEmpty()) {
+		if(db != null) {
+			if(!args.isEmpty())
+				loc = new BigInteger(args.get(0));
 			curRecord = db.getRecord(loc);
 			System.out.println("Read a record at location " + loc);
-			loc = loc.add(BigInteger.ONE);
-		} else if(args.size() == 1){
-			loc = new BigInteger(args.get(0));
-			curRecord = db.getRecord(loc);
-			System.out.println("Read a record at location " + loc);
+			System.out.println(curRecord);
 			loc = loc.add(BigInteger.ONE);
 		} else {
-			Util.fatalError("readRecord received too many arguments");
+			openDatabase();
+			System.out.println("GamesmanJava has opened the database file for you.");
+			u_readRecord(args);
 		}
 	}
 	
@@ -119,9 +130,26 @@ public class DatabaseModule extends UIModule {
 	//I don't know what you would put in to change the record.	
 	protected void u_editRecord(ArrayList<String> args) {
 		if(curRecord != null) {
-			
-		}
-	}		
+			if(!args.isEmpty()) {
+				if(args.get(0).toLowerCase().startsWith("r")) {
+					curRecord.set(RecordFields.REMOTENESS, Integer.parseInt(args.get(1)));
+					System.out.println("changed to: " + curRecord);
+				} else if(args.get(0).toLowerCase().startsWith("p")) {
+					curRecord.set(RecordFields.VALUE, Integer.parseInt(args.get(1)));
+					System.out.println("changed to: " + curRecord);
+				} else {
+					Util.fatalError("Invalid arguments to editRecord");
+				}
+			}
+		}		
+	}
+	
+	protected void u_initializeConfiguration(ArrayList<String> args) {
+		Game<?> g = Util.typedInstantiateArg("edu.berkeley.gamesman.game."+conf.getProperty("gamesman.game"), conf);
+		Hasher<?> h = Util.typedInstantiateArg("edu.berkeley.gamesman.hasher."+conf.getProperty("gamesman.hasher"), conf);
+		conf.initialize(g, h);
+		
+	}
 }
 
 /*
