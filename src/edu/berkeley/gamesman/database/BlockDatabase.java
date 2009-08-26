fa535793aa503cd9be0c28d758ac63baf6378594
dspyz <dspyz@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2009-08-26 20:24:58 +0000

    BlockDatabase compatible
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@4402 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/database/BlockDatabase.java
+++ src/edu/berkeley/gamesman/database/BlockDatabase.java
@@ -3,6 +3,7 @@ package edu.berkeley.gamesman.database;
 import java.io.IOException;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel.MapMode;
+import java.util.Iterator;
 
 import edu.berkeley.gamesman.core.RecordGroup;
 import edu.berkeley.gamesman.util.Util;
@@ -45,8 +46,8 @@ public class BlockDatabase extends FileDatabase {
 	public void initialize(String loc) {
 		super.initialize(loc);
 		try {
-			buf = fd.getChannel().map(MapMode.READ_WRITE, offset,
-					getByteSize());
+			buf = fd.getChannel()
+					.map(MapMode.READ_WRITE, offset, getByteSize());
 			buf.load();
 		} catch (IOException e) {
 			Util.fatalError("Could not map ByteBuffer from blockdb", e);
@@ -56,6 +57,31 @@ public class BlockDatabase extends FileDatabase {
 	@Override
 	public synchronized void putRecordGroup(long loc, RecordGroup value) {
 		buf.position((int) loc);
-		value.getState().outputUnsignedBytes(buf,conf.recordGroupByteLength);
+		value.getState().outputUnsignedBytes(buf, conf.recordGroupByteLength);
+	}
+
+	@Override
+	public Iterator<RecordGroup> getRecordGroups(long loc, int numGroups) {
+		groupsLength = numGroups * conf.recordGroupByteLength;
+		if (groups == null || groups.length < groupsLength)
+			groups = new byte[groupsLength];
+		buf.position((int) (loc + offset));
+		buf.get(groups);
+		RecordGroupByteIterator rgi = new RecordGroupByteIterator();
+		return rgi;
+	}
+
+	@Override
+	public void putRecordGroups(long loc, Iterator<RecordGroup> recordGroups,
+			int numGroups) {
+		groupsLength = numGroups * conf.recordGroupByteLength;
+		if (groups == null || groups.length < groupsLength)
+			groups = new byte[groupsLength];
+		int onByte = 0;
+		for (int i = 0; i < numGroups; i++)
+			recordGroups.next().getState().toUnsignedByteArray(groups, onByte,
+					conf.recordGroupByteLength);
+		buf.position((int) (loc + offset));
+		buf.put(groups);
 	}
 }
