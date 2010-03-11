a4f5d3a1135ab5034707d8b0cb8430ec30e304bf
dspyz <dspyz@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-03-11 05:44:20 +0000

    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@4705 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- testssh.sh
+++ testssh.sh
@@ -1,29 +1,60 @@
+echo akane.cs.berkeley.edu
+ssh akane.cs.berkeley.edu ls
+echo arlet.cs.berkeley.edu
 ssh arlet.cs.berkeley.edu ls
+echo barry.cs.berkeley.edu
 ssh barry.cs.berkeley.edu ls
+echo bonza.cs.berkeley.edu
 ssh bonza.cs.berkeley.edu ls
+echo braeburn.cs.berkeley.edu
 ssh braeburn.cs.berkeley.edu ls
+echo crispin.cs.berkeley.edu
 ssh crispin.cs.berkeley.edu ls
+echo davey.cs.berkeley.edu
 ssh davey.cs.berkeley.edu ls
+echo delicious.cs.berkeley.edu
 ssh delicious.cs.berkeley.edu ls
+echo dulcet.cs.berkeley.edu
 ssh dulcet.cs.berkeley.edu ls
+echo empress.cs.berkeley.edu
 ssh empress.cs.berkeley.edu ls
+echo fiesta.cs.berkeley.edu
 ssh fiesta.cs.berkeley.edu ls
+echo firmgold.cs.berkeley.edu
 ssh firmgold.cs.berkeley.edu ls
+echo freyburg.cs.berkeley.edu
 ssh freyburg.cs.berkeley.edu ls
+echo fuji.cs.berkeley.edu
 ssh fuji.cs.berkeley.edu ls
+echo geneva.cs.berkeley.edu
 ssh geneva.cs.berkeley.edu ls
+echo gilpin.cs.berkeley.edu
 ssh gilpin.cs.berkeley.edu ls
+echo grove.cs.berkeley.edu
 ssh grove.cs.berkeley.edu ls
+echo haas.cs.berkeley.edu
 ssh haas.cs.berkeley.edu ls
+echo hazen.cs.berkeley.edu
 ssh hazen.cs.berkeley.edu ls
+echo liberty.cs.berkeley.edu
 ssh liberty.cs.berkeley.edu ls
+echo mcintosh.cs.berkeley.edu
 ssh mcintosh.cs.berkeley.edu ls
+echo mio.cs.berkeley.edu
 ssh mio.cs.berkeley.edu ls
+echo orin.cs.berkeley.edu
 ssh orin.cs.berkeley.edu ls
+echo priam.cs.berkeley.edu
 ssh priam.cs.berkeley.edu ls
+echo shay.cs.berkeley.edu
 ssh shay.cs.berkeley.edu ls
+echo taylor.cs.berkeley.edu
 ssh taylor.cs.berkeley.edu ls
+echo vand.cs.berkeley.edu
 ssh vand.cs.berkeley.edu ls
+echo viking.cs.berkeley.edu
 ssh viking.cs.berkeley.edu ls
+echo winesap.cs.berkeley.edu
 ssh winesap.cs.berkeley.edu ls
+echo york.cs.berkeley.edu
 ssh york.cs.berkeley.edu ls
