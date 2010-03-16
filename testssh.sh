a606ced4363881c7d23bf4c1d7cb1033fa5d7732
dspyz <dspyz@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-03-16 07:02:49 +0000

    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@4737 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- testssh.sh
+++ testssh.sh
@@ -1,60 +1,60 @@
-echo akane.cs.berkeley.edu
-ssh akane.cs.berkeley.edu echo "Howdy"
-echo arlet.cs.berkeley.edu
-ssh arlet.cs.berkeley.edu echo "Howdy"
-echo barry.cs.berkeley.edu
-ssh barry.cs.berkeley.edu echo "Howdy"
-echo bonza.cs.berkeley.edu
-ssh bonza.cs.berkeley.edu echo "Howdy"
-echo braeburn.cs.berkeley.edu
-ssh braeburn.cs.berkeley.edu echo "Howdy"
-echo crispin.cs.berkeley.edu
-ssh crispin.cs.berkeley.edu echo "Howdy"
-echo davey.cs.berkeley.edu
-ssh davey.cs.berkeley.edu echo "Howdy"
-echo delicious.cs.berkeley.edu
-ssh delicious.cs.berkeley.edu echo "Howdy"
-echo dulcet.cs.berkeley.edu
-ssh dulcet.cs.berkeley.edu echo "Howdy"
-echo empress.cs.berkeley.edu
-ssh empress.cs.berkeley.edu echo "Howdy"
-echo fiesta.cs.berkeley.edu
-ssh fiesta.cs.berkeley.edu echo "Howdy"
-echo firmgold.cs.berkeley.edu
-ssh firmgold.cs.berkeley.edu echo "Howdy"
-echo freyburg.cs.berkeley.edu
-ssh freyburg.cs.berkeley.edu echo "Howdy"
-echo fuji.cs.berkeley.edu
-ssh fuji.cs.berkeley.edu echo "Howdy"
-echo geneva.cs.berkeley.edu
-ssh geneva.cs.berkeley.edu echo "Howdy"
-echo gilpin.cs.berkeley.edu
-ssh gilpin.cs.berkeley.edu echo "Howdy"
-echo grove.cs.berkeley.edu
-ssh grove.cs.berkeley.edu echo "Howdy"
-echo haas.cs.berkeley.edu
-ssh haas.cs.berkeley.edu echo "Howdy"
-echo hazen.cs.berkeley.edu
-ssh hazen.cs.berkeley.edu echo "Howdy"
-echo liberty.cs.berkeley.edu
-ssh liberty.cs.berkeley.edu echo "Howdy"
-echo mcintosh.cs.berkeley.edu
-ssh mcintosh.cs.berkeley.edu echo "Howdy"
-echo mio.cs.berkeley.edu
-ssh mio.cs.berkeley.edu echo "Howdy"
-echo orin.cs.berkeley.edu
-ssh orin.cs.berkeley.edu echo "Howdy"
-echo priam.cs.berkeley.edu
-ssh priam.cs.berkeley.edu echo "Howdy"
-echo shay.cs.berkeley.edu
-ssh shay.cs.berkeley.edu echo "Howdy"
-echo taylor.cs.berkeley.edu
-ssh taylor.cs.berkeley.edu echo "Howdy"
-echo vand.cs.berkeley.edu
-ssh vand.cs.berkeley.edu echo "Howdy"
-echo viking.cs.berkeley.edu
-ssh viking.cs.berkeley.edu echo "Howdy"
-echo winesap.cs.berkeley.edu
-ssh winesap.cs.berkeley.edu echo "Howdy"
-echo york.cs.berkeley.edu
-ssh york.cs.berkeley.edu echo "Howdy"
+echo akane
+ssh akane echo "Howdy"
+echo arlet
+ssh arlet echo "Howdy"
+echo barry
+ssh barry echo "Howdy"
+echo bonza
+ssh bonza echo "Howdy"
+echo braeburn
+ssh braeburn echo "Howdy"
+echo crispin
+ssh crispin echo "Howdy"
+echo davey
+ssh davey echo "Howdy"
+echo delicious
+ssh delicious echo "Howdy"
+echo dulcet
+ssh dulcet echo "Howdy"
+echo empress
+ssh empress echo "Howdy"
+echo fiesta
+ssh fiesta echo "Howdy"
+echo firmgold
+ssh firmgold echo "Howdy"
+echo freyburg
+ssh freyburg echo "Howdy"
+echo fuji
+ssh fuji echo "Howdy"
+echo geneva
+ssh geneva echo "Howdy"
+echo gilpin
+ssh gilpin echo "Howdy"
+echo grove
+ssh grove echo "Howdy"
+echo haas
+ssh haas echo "Howdy"
+echo hazen
+ssh hazen echo "Howdy"
+echo liberty
+ssh liberty echo "Howdy"
+echo mcintosh
+ssh mcintosh echo "Howdy"
+echo mio
+ssh mio echo "Howdy"
+echo orin
+ssh orin echo "Howdy"
+echo priam
+ssh priam echo "Howdy"
+echo shay
+ssh shay echo "Howdy"
+echo taylor
+ssh taylor echo "Howdy"
+echo vand
+ssh vand echo "Howdy"
+echo viking
+ssh viking echo "Howdy"
+echo winesap
+ssh winesap echo "Howdy"
+echo york
+ssh york echo "Howdy"
