59f6132c4f323a6c4a977d949d3654029c4a9cb9
dspyz <dspyz@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-04-05 15:57:42 +0000

    Does this fix it?
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@4773 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/JSONInterface.java
+++ src/edu/berkeley/gamesman/JSONInterface.java
@@ -145,13 +145,13 @@ public class JSONInterface extends GamesmanApplication {
 		}
 		Configuration conf = loadedConfigurations.get(filename);
 		if (conf != null) {
-			return conf.cloneAll();
+			return conf;
 		}
 		conf = addDatabase(params, game, filename);
 		if (conf != null) {
 			loadedConfigurations.put(filename, conf);
 		}
-		return conf.cloneAll();
+		return conf;
 	}
 
 	synchronized Configuration addDatabase(Map<String, String> params,
