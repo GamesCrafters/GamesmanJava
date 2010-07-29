67ab9af9a549027414320c982cd5d094e4386f16
dspyz <dspyz@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-07-29 00:28:00 +0000

    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@5357 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ clearDb.sh
@@ -0,0 +1,42 @@
+echo icluster2
+ssh icluster2.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster3
+ssh icluster3.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster4
+ssh icluster4.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster5
+ssh icluster5.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster6
+ssh icluster6.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster7
+ssh icluster7.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster8
+ssh icluster8.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster9
+ssh icluster9.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster10
+ssh icluster10.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster11
+ssh icluster11.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster12
+ssh icluster12.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster13
+ssh icluster13.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster14
+ssh icluster14.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster15
+ssh icluster15.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster16
+ssh icluster16.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster17
+ssh icluster17.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster18
+ssh icluster18.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster19
+ssh icluster19.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster20
+ssh icluster20.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster21
+ssh icluster21.eecs.berkeley.edu rm -rf /scratch/db76_fold
+echo icluster22
+ssh icluster22.eecs.berkeley.edu rm -rf /scratch/db76_fold
