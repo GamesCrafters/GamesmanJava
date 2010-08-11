15b62bd33a6674b4bcfeaee6448b4ff5409c6ad6
dspyz <dspyz@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-08-11 16:42:05 +0000

    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@5381 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/database/SplitDatabase.java
+++ src/edu/berkeley/gamesman/database/SplitDatabase.java
@@ -22,6 +22,7 @@ public class SplitDatabase extends Database {
 	private final ArrayList<Long> firstRecordList;
 	private final ArrayList<Long> numRecordsList;
 	private final boolean store;
+	private long size;
 
 	public SplitDatabase(String uri, Configuration conf, boolean solve,
 			long firstRecord, long numRecords, DatabaseHeader header) {
@@ -440,16 +441,37 @@ public class SplitDatabase extends Database {
 	}
 
 	public long getSize() {
-		long size = 0;
 		int percent = 0;
+		final SplitDatabase s = this;
+		Thread[] threads = new Thread[8];
 		for (int i = 0; i < databaseList.length; i++) {
-			Database d = databaseList[i];
+			final Database d = databaseList[i];
 			if (i * 100 / databaseList.length > percent) {
 				percent = i * 100 / databaseList.length;
 				System.out.println(percent + "%");
 			}
-			size += d.getSize();
+			if (threads[i & 7] != null && threads[i & 7].isAlive())
+				try {
+					threads[i & 7].join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			threads[i] = new Thread() {
+				public void run() {
+					long mySize = d.getSize();
+					synchronized (s) {
+						size += mySize;
+					}
+				}
+			};
+			threads[i].start();
 		}
+		for (Thread t : threads)
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		return size;
 	}
 }
