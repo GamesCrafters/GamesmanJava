98d9303f5b56efabe7094023ddf002c947f90452
petercheng00 <petercheng00@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-10-24 18:52:56 +0000

    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@5531 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/game/LoopyQuickCross.java
+++ src/edu/berkeley/gamesman/game/LoopyQuickCross.java
@@ -7,8 +7,8 @@ import edu.berkeley.gamesman.core.Configuration;
 import edu.berkeley.gamesman.core.Record;
 import edu.berkeley.gamesman.core.Value;
 import edu.berkeley.gamesman.game.util.BitSetBoard;
-import edu.berkeley.gamesman.hasher.ChangedIterator;
-import edu.berkeley.gamesman.hasher.DartboardHasher;
+//import edu.berkeley.gamesman.hasher.ChangedIterator;
+//import edu.berkeley.gamesman.hasher.DartboardHasher;
 
 public class LoopyQuickCross extends LoopyMutaGame {
 	private final int width;
@@ -48,6 +48,8 @@ public class LoopyQuickCross extends LoopyMutaGame {
 		return 0;
 	}
 
+	
+	//may be better to consolidate make move, changemove, etc. into valid moves, like in alignment.java
 	@Override
 	public boolean changeMove() {
 		// TODO Auto-generated method stub
@@ -75,10 +77,26 @@ public class LoopyQuickCross extends LoopyMutaGame {
 	}
 
 	@Override
-	//For this method, should we create a separate "QuickCrossHasher"?
+	//' ' = 0, '-' = 1, '|' = 2
 	public long getHash() {
-		// TODO Auto-generated method stub
-		return 0;
+		long retHash = 0;
+		int index = 0;
+		for(int x = 0; x < width; x++){
+			for(int y = 0; y < height; y++){
+				if (Board[x][y] == ' '){
+					//no change
+				}
+				else if(Board[x][y] == '-'){
+					retHash += Math.pow(3, index);
+				}
+				else if(Board[x][y] == '|'){
+					retHash += Math.pow(3, index) * 2;
+				}
+				else throw new Error("Error when hashing, bad piece");
+				index++;
+			}
+		}
+		return retHash;
 	}
 
 	@Override
@@ -107,8 +125,63 @@ public class LoopyQuickCross extends LoopyMutaGame {
 
 	@Override
 	public Value primitiveValue() {
-		// TODO Auto-generated method stub
-		return null;
+		//currently just setting everything to a win until I know what to do
+		char currPiece = '-';
+		
+		for (int i = 0; i<2; i++){
+			//checks for a vertical win
+			for (int row = 0; row < height; row++) {
+				int piecesInRow = 0;
+				for (int col = 0; col < width; col++) {
+					if (Board[row][col] == currPiece) {
+						piecesInRow++;
+						if (piecesInRow == piecesToWin)
+							return Value.WIN;
+					}
+					else
+						piecesInRow = 0;
+				}
+			}
+			
+			//checks for a horizontal win
+			for (int col = 0; col < width; col++) {
+				int piecesInCol = 0;
+				for (int row = 0; row < height; row++) {
+					if (Board[row][col] == currPiece) {
+						piecesInCol++;
+						if (piecesInCol == piecesToWin)
+							return Value.WIN;
+					} else
+						piecesInCol = 0;
+				}
+			}
+			//checks for diagonal win /
+			for (int row = 0; row <= height - piecesToWin; row++) {
+				for (int col = 0; col <= width - piecesToWin; col++) {
+					int pieces;
+					for (pieces = 0; pieces < piecesToWin; pieces++) {
+						if (Board[row + pieces][col + pieces] != currPiece)
+							break;
+					}
+					if (pieces == piecesToWin)
+						return Value.WIN;
+				}
+			}
+			//checks for diagonal win \
+			for (int row = 0; row <= height - piecesToWin; row++) {
+				for (int col = piecesToWin - 1; col < width; col++) {
+					int pieces;
+					for (pieces = 0; pieces < piecesToWin; pieces++) {
+						if (Board[row + pieces][col - pieces] != currPiece)
+							break;
+					}
+					if (pieces == piecesToWin)
+						return Value.WIN;
+				}
+			}
+			currPiece = '|';
+		}
+		return Value.UNDECIDED;
 	}
 
 	@Override
@@ -131,8 +204,23 @@ public class LoopyQuickCross extends LoopyMutaGame {
 
 	@Override
 	public void setToHash(long hash) {
-		// TODO Auto-generated method stub
-
+		long hashLeft = hash;
+		for (int index = width*height - 1; index >= 0; index--){
+			int y = index / 3;
+			int x = index % 3;
+			double base = Math.pow(3,index);
+			if (base > hashLeft){
+				Board[x][y] = ' ';
+			}
+			else if(base * 2 > hashLeft){
+				Board[x][y] = '-';
+				hashLeft = (long) (hashLeft - base);
+			}
+			else if(base * 2 <= hashLeft){
+				Board[x][y] = '|';
+				hashLeft = (long) (hashLeft - (base * 2));
+			}
+		}
 	}
 
 	@Override
