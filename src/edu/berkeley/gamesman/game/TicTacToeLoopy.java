c6b1947f688340d6425694c59c1276e91397822b
ninasnum <ninasnum@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-10-29 21:37:04 +0000

    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@5566 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ src/edu/berkeley/gamesman/game/TicTacToeLoopy.java
@@ -0,0 +1,39 @@
+package edu.berkeley.gamesman.game;
+
+import edu.berkeley.gamesman.core.Configuration;
+import edu.berkeley.gamesman.core.Record;
+import edu.berkeley.gamesman.core.Value;
+import edu.berkeley.gamesman.game.util.TierState;
+
+public class TicTacToeLoopy extends TicTacToe implements LoopyGame {
+
+	public TicTacToeLoopy(Configuration conf) {
+		super(conf);
+	}
+
+	@Override
+	public long recordStates() {
+		return super.recordStates() + 2;
+	}
+
+	@Override
+	public void longToRecord(TierState recordState, long record, Record toStore) {
+		if (record == super.recordStates()) {
+			toStore.value = Value.IMPOSSIBLE;
+		} else if (record == super.recordStates() + 1) {
+			toStore.value = Value.DRAW;
+		} else {
+			super.longToRecord(recordState, record, toStore);
+		}
+	}
+
+	@Override
+	public long recordToLong(TierState recordState, Record fromRecord) {
+		if (fromRecord.value == Value.IMPOSSIBLE)
+			return super.recordStates();
+		else if (fromRecord.value == Value.DRAW)
+			return super.recordStates() + 1;
+		else
+			return super.recordToLong(recordState, fromRecord);
+	}
+}
