0c3a33ab37800508d247b85d23315a4c9acfb523
ninasnum <ninasnum@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-11-01 19:21:18 +0000

    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@5586 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/game/LoopyGameWrapper.java
+++ src/edu/berkeley/gamesman/game/LoopyGameWrapper.java
@@ -1,12 +1,25 @@
 package edu.berkeley.gamesman.game;
 
 import java.util.Collection;
+import java.util.ArrayList;
 
 import edu.berkeley.gamesman.core.Configuration;
 import edu.berkeley.gamesman.core.Record;
 import edu.berkeley.gamesman.core.Value;
+import edu.berkeley.gamesman.util.qll.RecycleLinkedList;
+
+import edu.berkeley.gamesman.core.State;
+import edu.berkeley.gamesman.util.Pair;
+import edu.berkeley.gamesman.util.qll.Factory;
+import edu.berkeley.gamesman.util.qll.RLLFactory;
 
 public class LoopyGameWrapper extends LoopyMutaGame {
+//public final class LoopyGameWrapper<S extends State> extends LoopyMutaGame{
+//	private final Game<S> myGame;
+//	private final RecycleLinkedList<RecycleLinkedList<S>> moveLists;
+//	private final RecycleLinkedList<S> stateList;
+//	private final S[] possibleMoves;
+//	private final S[] startingPositions;
 
 	public LoopyGameWrapper(Configuration conf, LoopyGame g) {
 		super(conf);
@@ -97,6 +110,7 @@ public class LoopyGameWrapper extends LoopyMutaGame {
 	@Override
 	public void setStartingPosition(int i) {
 		// TODO Auto-generated method stub
+		setToState(startingPositions[i]);
 
 	}
 
@@ -115,25 +129,29 @@ public class LoopyGameWrapper extends LoopyMutaGame {
 	@Override
 	public String describe() {
 		// TODO Auto-generated method stub
-		return null;
+		//return null;
+		return myGame.describe();
 	}
 
 	@Override
 	public int maxChildren() {
 		// TODO Auto-generated method stub
-		return 0;
+		//return 0;
+		return myGame.maxChildren();
 	}
 
 	@Override
 	public long numHashes() {
 		// TODO Auto-generated method stub
-		return 0;
+		//return 0;
+		return myGame.numHashes();
 	}
 
 	@Override
 	public long recordStates() {
 		// TODO Auto-generated method stub
-		return 0;
+		//return 0;
+		return myGame.recordStates();
 	}
 
 }
