65314db5faf290728a15814ff0191b4bacb71bad
dspyz <dspyz@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-11-06 10:22:14 +0000

    Code added (but untested) for DartboardCache
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@5638 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/database/DartboardCache.java
+++ src/edu/berkeley/gamesman/database/DartboardCache.java
@@ -1,39 +1,105 @@
 package edu.berkeley.gamesman.database;
 
 import edu.berkeley.gamesman.core.Configuration;
-import edu.berkeley.gamesman.hasher.DartboardHasher;
 
 public class DartboardCache extends DatabaseWrapper {
 
-	public DartboardCache(Database db, String uri, Configuration config, long firstRecord, long numRecords, DartboardHasher dh) {
-		super(db, uri, config, true, firstRecord, numRecords);
-		//TODO Write constructor
+	MemoryDatabase[] slots;
+
+	public DartboardCache(Database db, String uri, Configuration conf,
+			long[] firstRecords, long[] lastRecords) {
+		super(db, uri, conf, true, firstFirst(firstRecords), maxNum(
+				firstRecords, lastRecords));
+		slots = new MemoryDatabase[firstRecords.length];
+		for (int i = 0; i < firstRecords.length; i++) {
+			slots[i] = new MemoryDatabase(db, null, conf, false, true);
+			slots[i].setRange(firstRecords[i],
+					(int) (lastRecords[i] - firstRecords[i]));
+		}
+	}
+
+	private static long maxNum(long[] firstRecords, long[] lastRecords) {
+		long max = Long.MIN_VALUE;
+		for (int i = 0; i < firstRecords.length; i++)
+			if (lastRecords[i] > max)
+				max = lastRecords[i];
+		return max + 1 - firstFirst(firstRecords);
+	}
+
+	private static long firstFirst(long[] firstRecords) {
+		long min = Long.MAX_VALUE;
+		for (long l : firstRecords)
+			if (l >= 0 && l < min)
+				min = l;
+		return min;
 	}
 
 	@Override
 	public void close() {
-		// TODO Auto-generated method stub
-
+		for (MemoryDatabase slot : slots)
+			slot.close();
 	}
 
 	@Override
 	protected void putBytes(DatabaseHandle dh, long loc, byte[] arr, int off,
 			int len) {
-		// TODO Auto-generated method stub
-
+		throw new Error("Cache is read only");
 	}
 
 	@Override
 	protected void getBytes(DatabaseHandle dh, long loc, byte[] arr, int off,
 			int len) {
-		// TODO Auto-generated method stub
+		getRecordsAsBytes(dh, loc, 0, arr, off, len, 0, true);
+	}
 
+	private static class DartCacheHandle extends DatabaseHandle {
+		protected DartCacheHandle(int recordGroupByteLength) {
+			super(recordGroupByteLength);
+		}
+
+		int whichDb;
+	}
+
+	@Override
+	public DatabaseHandle getHandle() {
+		DartCacheHandle result = new DartCacheHandle(recordGroupByteLength);
+		result.innerHandle = new DatabaseHandle(recordGroupByteLength);
+		return result;
+	}
+
+	@Override
+	protected void prepareRange(DatabaseHandle dh, long byteIndex,
+			int firstNum, long numBytes, int lastNum) {
+		super.prepareRange(dh, byteIndex, firstNum, numBytes, lastNum);
+		DartCacheHandle dc = (DartCacheHandle) dh;
+		for (int i = 0; i < slots.length; i++) {
+			if (slots[i].containsRecord(toFirstRecord(byteIndex) + firstNum)
+					&& slots[i].containsRecord(toFirstRecord(byteIndex
+							+ numBytes
+							- (lastNum > 0 ? recordGroupByteLength : 0))
+							+ lastNum - 1)) {
+				dc.whichDb = i;
+				slots[i].prepareRange(dc.innerHandle, byteIndex, firstNum,
+						numBytes, lastNum);
+				break;
+			}
+		}
+	}
+
+	@Override
+	protected int getBytes(DatabaseHandle dh, byte[] arr, int off, int maxLen,
+			boolean overwriteEdgesOk) {
+		DartCacheHandle dc = (DartCacheHandle) dh;
+		return slots[dc.whichDb].getBytes(dc.innerHandle, arr, off, maxLen,
+				overwriteEdgesOk);
 	}
 
 	@Override
 	public long getSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		long total = 0L;
+		for (MemoryDatabase slot : slots)
+			total += slot.getSize();
+		return total;
 	}
 
 }
