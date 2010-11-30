fd1fe5c57eda2ce3534f21bdbee72de8c6b50ee0
dspyz <dspyz@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-11-30 18:29:23 +0000

    Hadoop tier code added
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@5798 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ hadoop-src/edu/berkeley/gamesman/master/HadoopTierMapper.java
@@ -0,0 +1,47 @@
+package edu.berkeley.gamesman.master;
+
+import java.io.IOException;
+
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import edu.berkeley.gamesman.core.Configuration;
+import edu.berkeley.gamesman.core.GamesmanConf;
+
+public class HadoopTierMapper extends
+		Mapper<Range, NullWritable, FileStatus, Range> {
+	Configuration conf;
+	FileSystem fs;
+
+	@Override
+	public void setup(Context context) {
+		try {
+			org.apache.hadoop.conf.Configuration conf = context
+					.getConfiguration();
+			this.conf = new GamesmanConf(conf);
+			fs = FileSystem.get(conf);
+		} catch (ClassNotFoundException e) {
+			throw new Error(e);
+		} catch (IOException e) {
+			throw new Error(e);
+		}
+	}
+
+	@Override
+	public void map(Range key, NullWritable value, Context context)
+			throws IOException {
+		//TODO: Add tier slave stuff
+		FileStatus finalFile = null;
+		boolean successful = false;
+		while (!successful) {
+			try {
+				context.write(finalFile, key);
+				successful = true;
+			} catch (InterruptedException e) {
+				successful = false;
+			}
+		}
+	}
+}
---
+++ hadoop-src/edu/berkeley/gamesman/master/Range.java
@@ -0,0 +1,48 @@
+package edu.berkeley.gamesman.master;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.WritableComparable;
+
+public class Range implements WritableComparable<Range> {
+	long firstRecord;
+	long numRecords;
+
+	@Override
+	public void write(DataOutput out) throws IOException {
+		out.writeLong(firstRecord);
+		out.writeLong(numRecords);
+	}
+
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		firstRecord = in.readLong();
+		numRecords = in.readLong();
+	}
+
+	@Override
+	public int compareTo(Range o) {
+		if (firstRecord > o.firstRecord)
+			return 1;
+		else if (firstRecord < o.firstRecord)
+			return -1;
+		else if (numRecords > o.numRecords)
+			return 1;
+		else if (numRecords < o.numRecords)
+			return -1;
+		else
+			return 0;
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other instanceof Range) {
+			Range o = (Range) other;
+			return firstRecord == o.firstRecord && numRecords == o.numRecords;
+		} else
+			return false;
+	}
+
+}
