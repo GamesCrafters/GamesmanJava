4a70d68fa31899cddf323387b8a52e5810969501
pathorn <pathorn@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-12-23 04:57:47 +0000

    Moved 'junk' to 'scripts' to make its contents more obvious. Added missing job's
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@5930 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/util/JythonUtil.java
+++ src/edu/berkeley/gamesman/util/JythonUtil.java
@@ -11,7 +11,7 @@ import org.python.util.PythonInterpreter;
  * Utility function to load classes from .py files.
  */
 public class JythonUtil {
-	static String[] globalPath = {"jython_lib","junk","jobs","../GamesmanWeb/PythonPuzzles"};
+	static String[] globalPath = {"jython_lib","junk","scripts","jobs","../GamesmanWeb/PythonPuzzles"};
 	static {
 		PythonInterpreter.initialize(System.getProperties(), null, new String[0]);
 		for (String pathElement : globalPath) {
