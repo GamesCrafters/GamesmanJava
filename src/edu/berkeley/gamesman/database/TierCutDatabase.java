d8174e44ada80b268cb3db8c885e69f361388104
cmodic <cmodic@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2011-02-25 19:49:33 +0000

    TierCutDatabase simple layout
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@5976 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/database/TierCutDatabase.java
+++ src/edu/berkeley/gamesman/database/TierCutDatabase.java
@@ -5,9 +5,21 @@ import edu.berkeley.gamesman.game.TierGame;
 
 public class TierCutDatabase extends DatabaseWrapper {
 
+	private long myNumRecords;
+	TierGame myTierGame;
+	static boolean deleteLastRow;
 	public TierCutDatabase(Database db, String uri, Configuration config,
 			boolean solve, long firstRecord, long numRecords) {
 		super(db, uri, config, solve, firstRecord, numRecords);
+		myNumRecords = numRecords;
+	}
+	
+	public TierCutDatabase(Database db, String uri, Configuration config,
+			boolean solve, long firstRecord, long numRecords, TierGame myGame) {
+		super(db, uri, config, solve, firstRecord, numRecords);
+		myNumRecords = numRecords;
+		myTierGame = myGame;
+		deleteLastRow = true;
 	}
 
 	@Override
@@ -42,6 +54,11 @@ public class TierCutDatabase extends DatabaseWrapper {
 			return super.getBytes(dh, arr, off, maxLen, false);
 		int numBytes = (int) Math.min(maxLen, dh.lastByteIndex - dh.location);
 		// TODO Finish method
+		if(!shouldBeInDatabase(numBytes)) {
+			//Get Primitive Value instead
+		} else {
+			//Get from database
+		}
 		return numBytes;
 	}
 
@@ -52,19 +69,33 @@ public class TierCutDatabase extends DatabaseWrapper {
 			return super.putBytes(dh, arr, off, maxLen, false);
 		int numBytes = (int) Math.min(maxLen, dh.lastByteIndex - dh.location);
 		// TODO Finish method
+		if(!shouldBeInDatabase(numBytes)) {
+			//Put in database as normal
+		}
 		return numBytes;
 	}
 
 	@Override
 	public long getSize() {
 		// TODO Auto-generated method stub
-		return 0;
+		return myNumRecords;
 	}
+	
 
 	public static long getNumRecords(long firstRecord, long numRecords,
 			TierGame game) {
+		int numOfTiers = game.numberOfTiers();
+		long lastTierHashes = game.numHashesForTier(numOfTiers-1);
 		// TODO Auto-generated method stub
-		return 0;
+		if (deleteLastRow) {
+			return numRecords + lastTierHashes;
+		} else {
+			return numRecords;
+		}
+	}
+	
+	private boolean shouldBeInDatabase(int curByte) {
+		return curByte < myNumRecords;
 	}
 
 }
