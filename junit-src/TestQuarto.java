3e98ace43996a280b879da33f941962ec5e873d9
dspyz <dspyz@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2011-02-28 04:15:06 +0000

    Started working on cacher unit test but clearly cacher is broken
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@5984 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- junit-src/TestQuarto.java
+++ junit-src/TestQuarto.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import edu.berkeley.gamesman.hasher.QuartoMinorHasher;
 
 public class TestQuarto {
+	private static final long MEM = 256L;
+
 	@Test
 	public void testStepper() {
 		QuartoMinorHasher qmh = new QuartoMinorHasher();
@@ -42,6 +44,33 @@ public class TestQuarto {
 			if (qmh.getHash() % 3 == 0)
 				qmh.setTier(7);
 			qmh.hash(board);
+		}
+	}
+
+	@Test
+	public void testCacher() {
+		long[][][] caches = new long[8][17][];
+		QuartoMinorHasher qmh = new QuartoMinorHasher();
+		for (long hash = 0L; hash < 151410L; hash++) {
+			arbitraryReset(qmh);
+			for (int place = 0; place <= 7; place++) {
+				long[] oldCache = caches[place][16];
+				long[] cache = qmh.getCache(place, MEM);
+				if (!Arrays.equals(oldCache, cache)) {
+					Assert.assertTrue(oldCache == null
+							|| oldCache[1] == qmh.getHash());
+					System.out.println(qmh.getHash());
+					caches[place][16] = cache;
+				}
+				if (cache == null) {
+					for (int piece = 0; piece < 16; piece++) {
+						if (!qmh.used(piece)) {
+							caches[place][piece] = qmh.getCache(place, piece,
+									MEM / 11);
+						}
+					}
+				}
+			}
 		}
 	}
 }
