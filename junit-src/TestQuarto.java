8a544065fd657512ea7b080b04c6ddb4acdcc37d
dspyz <dspyz@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2011-03-01 18:13:08 +0000

    Quarto cacher unit test passes
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@5988 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- junit-src/TestQuarto.java
+++ junit-src/TestQuarto.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import edu.berkeley.gamesman.hasher.QuartoMinorHasher;
 
 public class TestQuarto {
-	private static final long MEM = 256L;
+	private static final long MEM = 512L;
 
 	@Test
 	public void testStepper() {
@@ -51,15 +51,16 @@ public class TestQuarto {
 	public void testChildren() {
 		QuartoMinorHasher qmh = new QuartoMinorHasher();
 		QuartoMinorHasher qmh2 = new QuartoMinorHasher();
-		int tier = 6;
+		int tier = 5;
 		qmh.setTier(tier);
 		qmh2.setTier(tier + 1);
-		long[] children = new long[72];
+		long[] children = new long[(tier + 1) * (16 - tier)];
 		long numHashes = qmh.numHashesForTier(tier);
 		for (long hash = 0L; hash < numHashes; hash++) {
 			arbitraryReset(qmh, tier);
 			int[] board = qmh.getBoard();
-			qmh.getChildren(children);
+			int numChildren = qmh.getChildren(children);
+			Assert.assertEquals(numChildren, children.length);
 			int[] board2 = new int[board.length + 1];
 			int childNum = 0;
 			for (int piece = 0; piece < 16; piece++) {
@@ -87,27 +88,74 @@ public class TestQuarto {
 
 	@Test
 	public void testCacher() {
-		long[][][] caches = new long[8][17][];
 		QuartoMinorHasher qmh = new QuartoMinorHasher();
-		for (long hash = 0L; hash < 151410L; hash++) {
-			arbitraryReset(qmh, 7);
-			for (int place = 0; place <= 7; place++) {
-				long[] oldCache = caches[place][16];
-				long[] cache = qmh.getCache(place, MEM);
-				if (!Arrays.equals(oldCache, cache)) {
-					Assert.assertTrue(oldCache == null
-							|| oldCache[1] == qmh.getHash());
-					caches[place][16] = cache;
-				}
-				if (cache == null) {
+		int tier = 5;
+		qmh.setTier(tier);
+		long[] children = new long[(tier + 1) * (16 - tier)];
+		long[][][] cache = new long[tier + 1][16][2];
+		long[][] fullCache = new long[tier + 1][];
+		long numHashes = qmh.numHashesForTier(tier);
+		for (long hash = 0L; hash < numHashes; hash++) {
+			arbitraryReset(qmh, tier);
+			for (int place = 0; place <= tier; place++) {
+				long[] oldCache = fullCache[place];
+				long[] newCache = qmh.getCache(place, MEM);
+				fullCache[place] = newCache;
+				if (newCache == null) {
 					for (int piece = 0; piece < 16; piece++) {
-						if (!qmh.used(piece)) {
-							caches[place][piece] = qmh.getCache(place, piece,
-									MEM / 11);
+						if (qmh.used(piece))
+							continue;
+						newCache = qmh
+								.getCache(place, piece, MEM / (16 - tier));
+						long[] oldInnerCache = cache[place][piece];
+						if (!Arrays.equals(oldInnerCache, newCache)) {
+							if (oldInnerCache == null) {
+								oldInnerCache = oldCache;
+							}
+							Assert.assertTrue(newCache[0] + newCache[1] <= oldInnerCache[0]
+									|| newCache[0] >= oldInnerCache[0]
+											+ oldInnerCache[1]);
+							cache[place][piece] = newCache;
+						}
+					}
+				} else {
+					if (!Arrays.equals(oldCache, newCache)) {
+						if (oldCache != null) {
+							Assert.assertTrue(newCache[0] + newCache[1] <= oldCache[0]
+									|| newCache[0] >= oldCache[0] + oldCache[1]);
 						}
 					}
 				}
 			}
+			int[] board = qmh.getBoard();
+			int numChildren = qmh.getChildren(children);
+			Assert.assertEquals(numChildren, children.length);
+			int[] board2 = new int[board.length + 1];
+			int childNum = 0;
+			for (int piece = 0; piece < 16; piece++) {
+				if (qmh.used(piece))
+					continue;
+				for (int place = 0; place <= board.length; place++) {
+					long child = children[childNum++];
+					for (int i = 0; i < board2.length; i++) {
+						if (i < place) {
+							board2[i] = board[i];
+						} else if (i > place) {
+							board2[i] = board[i - 1];
+						} else
+							board2[i] = piece;
+					}
+					long[] childCache;
+					if (fullCache[place] == null)
+						childCache = cache[place][piece];
+					else
+						childCache = fullCache[place];
+					long ind = child - childCache[0];
+					Assert.assertTrue(ind >= 0 && ind < childCache[1]);
+				}
+			}
+			if (hash < numHashes - 1L)
+				qmh.nextHashInTier();
 		}
 	}
 }
