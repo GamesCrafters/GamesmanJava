de76bc0678797d945ae5a596ecff65c98b4b2c86
cmodic <cmodic@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2011-03-11 20:07:55 +0000

    TierCut modified for compatibility, and for actions to be done on record level
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@6014 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/database/TierCutDatabase.java
+++ src/edu/berkeley/gamesman/database/TierCutDatabase.java
@@ -1,89 +1,47 @@
 package edu.berkeley.gamesman.database;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Iterator;
+import java.math.BigInteger;
+import java.util.ArrayList;
 
 import edu.berkeley.gamesman.core.Configuration;
 import edu.berkeley.gamesman.core.Record;
 import edu.berkeley.gamesman.core.Value;
-import edu.berkeley.gamesman.database.wrapper.DatabaseWrapper;
 import edu.berkeley.gamesman.game.TierGame;
 import edu.berkeley.gamesman.game.util.TierState;
 import edu.berkeley.gamesman.util.Pair;
 
-public class TierCutDatabase extends DatabaseWrapper {
+public class TierCutDatabase extends Database {
+
 	
 	TierGame myTierGame;
 	private static boolean deleteLastRow;
 	private static int numTiersCut;
 
-	public TierCutDatabase(Database db, Configuration config, long firstRecord,
-			long numRecords, boolean reading, boolean writing) {
-		super(db, config, firstRecord, numRecords, reading, writing);
-		myTierGame = (TierGame) config.getGame();
+	public TierCutDatabase(Configuration conf, long firstRecordIndex, long numRecords,
+			boolean reading, boolean writing) {
+		super(conf,firstRecordIndex, numRecords,
+				reading, writing);
+		myTierGame = (TierGame) conf.getGame();
 		deleteLastRow = true;
 		numTiersCut = 1;
 	}
 
-	@Override
-	public void close() {
-		// TODO Auto-generated method stub
-
-	}
 
 	@Override
-	protected int writeBytes(DatabaseHandle dh, long location, byte[] array,
-			int off, int len) throws IOException {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	protected int readBytes(DatabaseHandle dh, long location, byte[] array,
-			int off, int len) throws IOException {
-		throw new UnsupportedOperationException();
-	}
-
-	/*
-	 * @Override protected synchronized void prepareRange(DatabaseHandle dh,
-	 * long byteIndex, int firstNum, long numBytes, int lastNum) {
-	 * super.prepareRange(dh, byteIndex, firstNum, numBytes, lastNum); // TODO
-	 * Finish method }
-	 * 
-	 * @Override protected synchronized int getBytes(DatabaseHandle dh, byte[]
-	 * arr, int off, int maxLen, boolean overwriteEdgesOk) { if
-	 * (!overwriteEdgesOk) return super.getBytes(dh, arr, off, maxLen, false);
-	 * int numBytes = (int) Math.min(maxLen, dh.lastByteIndex - dh.location); //
-	 * TODO Finish method if (!shouldBeInDatabase(numBytes)) { // Get Primitive
-	 * Value instead int hash = off; //get hash from input is off? TierState pos
-	 * = new TierState(); myTierGame.hashToState(hash, pos);
-	 * myTierGame.setState(pos); Record myRecord = new Record(conf);
-	 * myRecord.value = myTierGame.primitiveValue(); myRecord.remoteness = 0;
-	 * long val = myTierGame.recordToLong(new TierState(), myRecord);
-	 * toUnsignedByteArray(val,arr, off); // is off right? } else { // Get from
-	 * database as normal return super.getBytes(dh, arr, off, maxLen,
-	 * overwriteEdgesOk); //? } return numBytes; }
-	 * 
-	 * @Override protected synchronized int putBytes(DatabaseHandle dh, byte[]
-	 * arr, int off, int maxLen, boolean edgesAreCorrect) { if
-	 * (!edgesAreCorrect) return super.putBytes(dh, arr, off, maxLen, false);
-	 * int numBytes = (int) Math.min(maxLen, dh.lastByteIndex - dh.location); if
-	 * (shouldBeInDatabase(numBytes)) { return super.putBytes(dh, arr, off,
-	 * maxLen, false); } return numBytes; }
-	 */
-
-	@Override
-	public long readRecord(DatabaseHandle dh, long recordIndex)
-			throws IOException {
-		long byteIndex = myLogic.getByteIndex(recordIndex);
-		TierState curPos = myTierGame.hashToState(recordIndex);
+	public long readRecord(DatabaseHandle dh, long recordIndex) throws IOException {
+		TierState curPos = new TierState();
+		curPos = myTierGame.hashToState(recordIndex);
 		myTierGame.setState(curPos);
 		int tier = myTierGame.getTier();
 		if (shouldBeInDatabase(tier)) {
-			return readRecordFromByteIndex(dh, byteIndex);
+			return readRecordFromByteIndex(dh, myLogic.getByteIndex(recordIndex));
 		} else {
-			if (inLastRow(byteIndex)) {
-				Record myRecord = myTierGame.newRecord();
+			if (inLastRow(recordIndex)) {
+				TierState pos = new TierState();
+				pos = myTierGame.hashToState(recordIndex);
+				myTierGame.setState(pos);
+				Record myRecord = new Record(conf);
 				myRecord.value = myTierGame.primitiveValue();
 				myRecord.remoteness = 0;
 				long val = myTierGame.recordToLong(myRecord);
@@ -95,18 +53,19 @@ public class TierCutDatabase extends DatabaseWrapper {
 		}
 	}
 
-	private long missingTierSolve(DatabaseHandle dh, long recordIndex)
-			throws IOException {
-		TierState pos = myTierGame.hashToState(recordIndex);
-		Collection<Pair<String, TierState>> moves = myTierGame.validMoves(pos);
+	private long missingTierSolve(DatabaseHandle dh, long recordIndex) throws IOException {
+		long hash = recordIndex;
+		TierState pos = new TierState();
+		pos = myTierGame.hashToState(hash);
+		ArrayList<Pair<String, TierState>> moves = (ArrayList) myTierGame
+				.validMoves(pos);
 		boolean hasTie = false;
 		boolean hasWin = false;
 		Record myRecord = new Record(conf);
 		int currentRemoteness = Integer.MAX_VALUE;
 		boolean gameHasRemoteness = false;
-		Iterator<Pair<String, TierState>> moveIter = moves.iterator();
 		for (int i = 0; i < moves.size(); i++) {
-			Pair<String, TierState> move = moveIter.next();
+			Pair<String, TierState> move = moves.get(i);
 			TierState newState = myTierGame.doMove(pos, move.car);
 			long newHash = myTierGame.stateToHash(newState);
 			Record curRecord = new Record(conf);
@@ -146,7 +105,7 @@ public class TierCutDatabase extends DatabaseWrapper {
 		} else {
 			myRecord.value = Value.LOSE;
 		}
-		if (gameHasRemoteness) {
+		if(gameHasRemoteness) {
 			myRecord.remoteness = currentRemoteness + 1;
 		}
 		long val = myTierGame.recordToLong(myRecord);
@@ -154,16 +113,16 @@ public class TierCutDatabase extends DatabaseWrapper {
 	}
 
 	@Override
-	public void writeRecord(DatabaseHandle dh, long recordIndex, long r)
-			throws IOException {
-		long byteIndex = myLogic.getByteIndex(recordIndex);
-		TierState curPos = myTierGame.hashToState(recordIndex);
+	public void writeRecord(DatabaseHandle dh, long recordIndex, long r) throws IOException {
+		TierState curPos = new TierState();
+		curPos = myTierGame.hashToState(recordIndex);
 		myTierGame.setState(curPos);
 		int tier = myTierGame.getTier();
 		if (shouldBeInDatabase(tier)) {
-			writeRecordFromByteIndex(dh, byteIndex, r);
+			writeRecordFromByteIndex(dh, myLogic.getByteIndex(recordIndex), recordIndex);
 		}
 	}
+
 
 	// Is this supposed to be the actual amount of records, or the lying amount
 	// of records?
@@ -171,7 +130,6 @@ public class TierCutDatabase extends DatabaseWrapper {
 			TierGame game) {
 		int numOfTiers = game.numberOfTiers();
 		long lastTierHashes = game.numHashesForTier(numOfTiers - 1);
-		long currentSize = 0;
 		// TODO Auto-generated method stub
 		if (deleteLastRow) {
 			return numRecords + lastTierHashes;
@@ -208,4 +166,19 @@ public class TierCutDatabase extends DatabaseWrapper {
 						.numHashesForTier(myTierGame.numberOfTiers() - 1));
 
 	}
+
+	@Override
+	protected int readBytes(DatabaseHandle dh, long location, byte[] array,
+			int off, int len) throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected int writeBytes(DatabaseHandle dh, long location, byte[] array,
+			int off, int len) throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
