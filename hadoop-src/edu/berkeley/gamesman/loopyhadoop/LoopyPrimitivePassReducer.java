eb28282267601759e9ae5419ac4400d162c7a698
dshoe1989 <dshoe1989@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2011-03-20 20:07:07 +0000

    [STAGE2] Closed the reader in Mapper and also wrote the basic structure for Reducer
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@6049 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- hadoop-src/edu/berkeley/gamesman/loopyhadoop/LoopyPrimitivePassReducer.java
+++ hadoop-src/edu/berkeley/gamesman/loopyhadoop/LoopyPrimitivePassReducer.java
@@ -1,124 +1,102 @@
 package edu.berkeley.gamesman.loopyhadoop;
 
-import java.io.IOException;
-import java.util.Random;
-
 import edu.berkeley.gamesman.core.Configuration;
 import edu.berkeley.gamesman.core.Record;
 import edu.berkeley.gamesman.core.State;
 import edu.berkeley.gamesman.core.Value;
-import edu.berkeley.gamesman.database.DatabaseHandle;
-import edu.berkeley.gamesman.database.FileDatabase;
 import edu.berkeley.gamesman.game.Game;
+import edu.berkeley.gamesman.parallel.Range;
 import edu.berkeley.gamesman.parallel.RangeFile;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.LocalFileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: dxu
+ * Date: 3/18/11
+ * Time: 12:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
 public class LoopyPrimitivePassReducer<S extends State> extends
-		Reducer<RangeFile, LongWritable, LongWritable, IntWritable> {
-	private FileSystem fs;
-	private Configuration conf;
-	private Game<S> game;
-	private final Random rand = new Random();
-	private IntWritable zero = new IntWritable(0);
+        Reducer<RangeFile, LongWritable, LongWritable, IntWritable> {
 
-	@Override
-	public void setup(Context context) {
-		try {
-			org.apache.hadoop.conf.Configuration hadoopConf = context
-					.getConfiguration();
-			conf = Configuration.deserialize(hadoopConf
-					.get("gamesman.configuration"));
-			fs = FileSystem.get(hadoopConf);
-			game = conf.getCheckedGame();
-		} catch (IOException e) {
-			throw new Error(e);
-		} catch (ClassNotFoundException e) {
-			throw new Error(e);
-		}
-	}
+    private FileSystem fs;
+    private Configuration conf;
+    private Game<S> game;
+    private final Random random = new Random();
+    private Path dbFolder;
+    private LongWritable returnKey;
+    private IntWritable returnVal;
 
-	@Override
-	public void reduce(RangeFile rangeFile, Iterable<LongWritable> hashes,
-			Context context) {
-		Path path = rangeFile.myFile.getPath();
-		try {
-			LocalFileSystem lfs = new LocalFileSystem();
-			String stringPath = lfs.pathToFile(path).getPath();
-			String localStringPath = stringPath + "_local";
-			Path localPath = new Path(localStringPath);
-			fs.copyToLocalFile(path, localPath);
-			FileDatabase database = new FileDatabase(localStringPath);
-			DatabaseHandle readHandle = database.getHandle(true);
-			DatabaseHandle writeHandle = database.getHandle(false);
-			Record record = game.newRecord();
-			S gameState = game.newState();
+    @Override
+    public void setup(Context context) {
+        try {
+            org.apache.hadoop.conf.Configuration hadoopConf = context
+                    .getConfiguration();
+            conf = Configuration.deserialize(hadoopConf.get("gamesman.configuration"));
+            game = conf.getCheckedGame();
+            dbFolder = new Path(conf.getProperty("gamesman.hadoop.dbfolder"));
+            fs = FileSystem.get(hadoopConf);
+            returnKey = new LongWritable();
+            returnVal = new IntWritable();
 
-			/*
-			 * all hashes are going to be in the same file so this goes outside
-			 * the loop
-			 */
-			boolean changesMade = false;
-			for (LongWritable hash : hashes) {
-				long longHash = hash.get();
-				game.hashToState(longHash, gameState);
-				long recordHash = database.readRecord(readHandle, longHash);
-				game.longToRecord(gameState, recordHash, record);
-				/* get the record associated with this hash */
+            //Donno if this is necessary
+            returnVal.set(0);
+        } catch (IOException e) {
+            throw new Error(e);
+        } catch (ClassNotFoundException e) {
+            throw new Error(e);
+        }
+    }
 
-				boolean visited = record.value != Value.IMPOSSIBLE;
+    @Override
+    public void reduce(RangeFile file, Iterable<LongWritable> hashes, Context context) {
+        FileStatus myFileStatus = file.myFile;
+        for (LongWritable lw : hashes) {
+            Long hash = lw.get();
+            Record hashVal = null;
+            Value v;
+            //TODO: Read HashVal from the database
 
-				if (!visited) {
-					Value primitiveValue = game.primitiveValue(game
-							.hashToState(longHash));
-					switch (primitiveValue) {
-					case UNDECIDED:
-						// value is not primitive
-						record.value = Value.DRAW;
-						break;
-					case WIN:
-					case LOSE:
-					case TIE:
-						// do the same for all these cases, all primitive
-						// TODO: append to a primitive file for this range
-						record.value = primitiveValue;
-						record.remoteness = 0;
-						// we have to deal with this during the solve for
-						// non-primitives?
-						break;
-					default:
-						throw new Error("WTF did primitive value return?");
-					}
+            try {
+                if (hashVal.value != Value.IMPOSSIBLE) {
+                    //It was visited Already
+                    continue;
+                }
 
-					recordHash = game.recordToLong(gameState, record);
-					database.writeRecord(writeHandle, longHash, recordHash);
-					// write this record to the database
-					changesMade = true;
-					context.write(hash, zero);
-				}
-			}
+                //Check Primitive
+                S state = game.hashToState(hash);
+                v = game.primitiveValue(state);
+                //TODO: Mark the Value in the DB
 
-			database.close();
 
-			if (changesMade) {
-				Path tempPath = new Path(stringPath + "_" + rand.nextLong());
-				// use a random long to prevent collisions in the expensive copy
-				// step
-				fs.moveFromLocalFile(localPath, tempPath);
-				// copy the written database to hdfs
-				fs.rename(tempPath, path);
-				// rename to complete process
-			}
-		} catch (IOException e) {
-			throw new Error(e);
-		} catch (ClassNotFoundException e) {
-			throw new Error(e);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-}
+                //It is Primitive
+                if (v != Value.UNDECIDED) {
+                    //TODO: Write to the PrimitiveValues SeqFile
+                //Not Primitive
+                } else {
+
+                    returnKey.set(hash);
+                    context.write(returnKey, returnVal);
+                }
+            } catch (InterruptedException e) {
+                throw new Error(e);
+            } catch (IOException e) {
+                throw new Error(e);
+            }
+        }
+    }
+
+
+    }
