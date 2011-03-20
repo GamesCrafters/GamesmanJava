82986236ffe1a342283bfa5606663b6a28c31f3b
vesuvius <vesuvius@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2011-03-20 01:50:15 +0000

    Reducer progress for the mark positions pass in the loopy solver.  Still not generating the primitives.
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@6044 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- hadoop-src/edu/berkeley/gamesman/loopyhadoop/LoopyPrimitivePassReducer.java
+++ hadoop-src/edu/berkeley/gamesman/loopyhadoop/LoopyPrimitivePassReducer.java
@@ -25,6 +25,7 @@ public class LoopyPrimitivePassReducer<S extends State> extends
 	private Configuration conf;
 	private Game<S> game;
 	private final Random rand = new Random();
+	private IntWritable zero = new IntWritable(0);
 
 	@Override
 	public void setup(Context context) {
@@ -60,43 +61,103 @@ public class LoopyPrimitivePassReducer<S extends State> extends
 			DatabaseHandle writeHandle = readDatabase.getHandle(false);
 			Record record = game.newRecord();
 			S gameState = game.newState();
+			
+			/*
+			 * all hashes are going to be in the same file so this goes outside
+			 * the loop
+			 */
+			boolean changesMade = false;
+			for (LongWritable hash : hashes) {
+				long longHash = hash.get();
+				gameState = game.hashToState(longHash);
+				long recordHash = readDatabase.readRecord(readHandle, longHash);
+				game.longToRecord(gameState, recordHash, record);
+				/* get the record associated with this hash */
 
-			// How to read values out of the database
-			long exampleHash = 0L;
-			gameState = game.hashToState(exampleHash);
-			long recordHash = readDatabase.readRecord(readHandle, exampleHash);
-			game.longToRecord(gameState, recordHash, record);
+				boolean visited = record.value == Value.IMPOSSIBLE;
 
-			// Value is now stored in record. To check if it's been seen:
-			boolean seenAlready = record.value == Value.IMPOSSIBLE;
-
-			// To get primitive value for this position, do:
-			Value primitive = game.primitiveValue(gameState);
-			// If it's not primitive, this will return Value.UNDECIDED
-
-			// Setting record fields
-			record.value = primitive; // Or Draw if primitive is UNDECIDED
-			record.remoteness = 0;
-			// If remoteness doesn't make sense (such as with DRAW or
-			// IMPOSSIBLE), then don't worry about it
-
-			// To store values you just do:
-			recordHash = game.recordToLong(gameState, record);
-			writeDatabase.writeRecord(writeHandle, exampleHash, recordHash);
-
-			// When you're all done
+				if (!visited) {
+					Value primitiveValue = game.primitiveValue(game
+							.hashToState(longHash));
+					switch (primitiveValue) {
+					case UNDECIDED:
+						// value is not primitive
+						record.value = Value.DRAW;
+						record.remoteness = -1;//set as an error flag?
+						break;
+					case WIN:
+					case LOSE:
+					case TIE:
+						// do the same for all these cases, all primitive
+						// TODO: append to a primitive file for this range
+						record.value = primitiveValue;
+						record.remoteness = 0;//we have to deal with this during the solve for non-primitives?
+						break;
+					default:
+						throw new Error("WTF did primitive value return?");
+					}
+					
+					recordHash = game.recordToLong(gameState, record);
+					writeDatabase.writeRecord(writeHandle, longHash, recordHash);//write this record to the hash
+					
+					changesMade = true;
+					context.write(hash, zero);
+				}
+			}
+			
 			readDatabase.close();
 			writeDatabase.close();
+			
+			if(changesMade)
+			{
+				Path tempPath = new Path(stringPath + "_" + rand.nextLong());
+				//use a random long to prevent collisions in the expensive copy step
+				fs.moveFromLocalFile(new Path(newPath), tempPath);
+				//copy the written database to hdfs
+				fs.rename(tempPath, path);
+				//rename to complete process
+			}
+			
 
-			// And if you made any changes:
-			Path tempPath = new Path(stringPath + "_" + rand.nextLong());
-			fs.moveFromLocalFile(new Path(newPath), tempPath);
-			fs.rename(tempPath, path);
+			// // How to read values out of the database
+			// long exampleHash = 0L;
+			// gameState = game.hashToState(exampleHash);
+			// long recordHash = readDatabase.readRecord(readHandle,
+			// exampleHash);
+			// game.longToRecord(gameState, recordHash, record);
+			//
+			// // Value is now stored in record. To check if it's been seen:
+			// boolean seenAlready = record.value == Value.IMPOSSIBLE;
+			//
+			// // To get primitive value for this position, do:
+			// Value primitive = game.primitiveValue(gameState);
+			// // If it's not primitive, this will return Value.UNDECIDED
+			//
+			// // Setting record fields
+			// record.value = primitive; // Or Draw if primitive is UNDECIDED
+			// record.remoteness = 0;
+			// // If remoteness doesn't make sense (such as with DRAW or
+			// // IMPOSSIBLE), then don't worry about it
+			//
+			// // To store values you just do:
+			// recordHash = game.recordToLong(gameState, record);
+			// writeDatabase.writeRecord(writeHandle, exampleHash, recordHash);
+			//
+			// // When you're all done
+			// readDatabase.close();
+			// writeDatabase.close();
+			//
+			// // And if you made any changes:
+			// Path tempPath = new Path(stringPath + "_" + rand.nextLong());
+			// fs.moveFromLocalFile(new Path(newPath), tempPath);
+			// fs.rename(tempPath, path);
 
 		} catch (IOException e) {
 			throw new Error(e);
 		} catch (ClassNotFoundException e) {
 			throw new Error(e);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 }
