3e4f00160867a2a13907e05d107af168d3fbf5b7
dspyz <dspyz@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2011-03-23 03:36:03 +0000

    Quarto Cache progress
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@6060 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/database/cache/QuartoCache.java
+++ src/edu/berkeley/gamesman/database/cache/QuartoCache.java
@@ -1,7 +1,5 @@
 package edu.berkeley.gamesman.database.cache;
 
-import java.io.IOException;
-
 import edu.berkeley.gamesman.core.Record;
 import edu.berkeley.gamesman.database.Database;
 import edu.berkeley.gamesman.game.Quarto;
@@ -15,6 +13,7 @@ public class QuartoCache extends TierCache {
 	private final DartboardHasher majorHasher;
 	private final QuartoMinorHasher minorHasher;
 	private final Quarto game;
+	private final long cacheMemory;
 
 	public QuartoCache(Quarto game, DartboardHasher majorHasher,
 			QuartoMinorHasher minorHasher, Database db, long availableMemory) {
@@ -26,15 +25,50 @@ public class QuartoCache extends TierCache {
 			for (int pieceNum = 0; pieceNum < 16; pieceNum++) {
 				upperCaches[place][pieceNum] = new RecordRangeCache(db);
 			}
-			lowerCache[place] = new RecordRangeCache(db);
+			lowerCache[place] = upperCaches[place][0];
 		}
+		cacheMemory = availableMemory / 256;
 	}
 
 	@Override
 	public void fetchChildren(TierState position, int numChildren,
 			TierState[] children, int[] hints, Record[] values) {
-		// TODO Auto-generated method stub
+		for (int child = 0; child < numChildren; child++) {
+			int piece = hints[child] / 16;
+			int place = hints[child] % 16;
+			long childHash = game.stateToHash(children[child]);
+			boolean childFetched = fetchChild(children[child], values[child],
+					piece, place, childHash);
+			if (!childFetched) {
+				if (!(setCacheThroughAll(place, cacheMemory) || setCacheThrough(
+						place, cacheMemory))) {
+					setCacheThrough(place, piece, cacheMemory);
+				}
+				childFetched = fetchChild(children[child], values[child],
+						piece, place, childHash);
+				assert childFetched;
+			}
+		}
+	}
 
+	private boolean fetchChild(TierState childState, Record toStore, int piece,
+			int place, long childHash) {
+		if (fetchChild(childState, lowerCache[place], childHash, toStore))
+			return true;
+		else if (fetchChild(childState, upperCaches[place][piece], childHash,
+				toStore))
+			return true;
+		else
+			return false;
+	}
+
+	private boolean fetchChild(TierState childState, RecordRangeCache cache,
+			long childHash, Record toStore) {
+		if (cache.containsRecord(childHash)) {
+			game.longToRecord(childState, cache.readRecord(childHash), toStore);
+			return true;
+		} else
+			return false;
 	}
 
 	private boolean setCacheThrough(int place, long availableMemory) {
@@ -46,14 +80,30 @@ public class QuartoCache extends TierCache {
 		if (range == null)
 			return false;
 		else {
-			lowerCache[place].ensureByteCapacity(availableIntMemory, false);
-			lowerCache[place].setRange(
-					game.hashOffsetForTier()
-							+ majorHasher.getChild(' ', 'P', place)
-							* minorHasher.numHashesForTier() + range[0],
-					(int) range[1]);
+			setCacheMinorRange(lowerCache[place], place, range[0],
+					(int) range[1], availableIntMemory);
 			return true;
 		}
+	}
+
+	private void setCacheMinorRange(RecordRangeCache cache, int place,
+			long minorRecordIndex, int numRecords, int ensureBytes) {
+		cache.ensureByteCapacity(ensureBytes, false);
+		cache.setRange(
+				game.hashOffsetForTier(game.getTier() + 1)
+						+ majorHasher.nextChild(' ', 'P', place)
+						* minorHasher.numHashesForTier(game.getTier() + 1)
+						+ minorRecordIndex, numRecords);
+	}
+
+	private void setCacheMajorRange(RecordRangeCache cache, int place,
+			long majorRecordIndex, int numMajorPlaces, int ensureBytes) {
+		cache.ensureByteCapacity(ensureBytes, false);
+		long nextTierNumHashes = minorHasher
+				.numHashesForTier(game.getTier() + 1);
+		cache.setRange(game.hashOffsetForTier(game.getTier() + 1)
+				+ majorRecordIndex * nextTierNumHashes,
+				(int) (numMajorPlaces * nextTierNumHashes));
 	}
 
 	private boolean setCacheThrough(int place, int piece, long availableMemory) {
@@ -65,13 +115,8 @@ public class QuartoCache extends TierCache {
 		if (range == null)
 			return false;
 		else {
-			upperCaches[place][piece].ensureByteCapacity(availableIntMemory,
-					false);
-			upperCaches[place][piece].setRange(
-					game.hashOffsetForTier()
-							+ majorHasher.getChild(' ', 'P', place)
-							* minorHasher.numHashesForTier() + range[0],
-					(int) range[1]);
+			setCacheMinorRange(upperCaches[place][piece], place, range[0],
+					(int) range[1], availableIntMemory);
 			return true;
 		}
 	}
@@ -93,8 +138,11 @@ public class QuartoCache extends TierCache {
 				majorHasher.unhash(majorHash + addMajor - 1);
 				lastMajorChild = majorHasher.previousChild(' ', 'P', place);
 				addMajor /= 2;
-			} while (lastMajorChild - currentMajorChild + 1 > addMajor);
+			} while (lastMajorChild - currentMajorChild + 1 > availableMajor);
 			majorHasher.unhash(majorHash);
+			setCacheMajorRange(lowerCache[place], place, currentMajorChild,
+					(int) (lastMajorChild - currentMajorChild + 1),
+					availableIntMemory);
 			return true;
 		}
 	}
