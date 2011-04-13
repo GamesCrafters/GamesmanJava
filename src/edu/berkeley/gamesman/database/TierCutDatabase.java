b149bd19f1a75c076c8713ba8c4b7acc535d4aaf
dspyz <dspyz@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2011-04-13 18:41:38 +0000

    TierCutDatabase working
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@6138 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/gamesman/database/TierCutDatabase.java
+++ src/edu/berkeley/gamesman/database/TierCutDatabase.java
@@ -1,34 +1,71 @@
 package edu.berkeley.gamesman.database;
 
-import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
 
 import edu.berkeley.gamesman.core.Configuration;
 import edu.berkeley.gamesman.core.Record;
+import edu.berkeley.gamesman.core.Value;
 import edu.berkeley.gamesman.game.TierGame;
 import edu.berkeley.gamesman.game.util.TierState;
 import edu.berkeley.gamesman.util.Pair;
+import edu.berkeley.gamesman.util.Util;
 
 public final class TierCutDatabase extends Database {
 
+	private final String uri;
 	private final TierGame myTierGame;
-	private final boolean deleteLastRow;
-	private final int numTiersCut;
-	private SplitLocalDatabase inner;
+	private final boolean[] inDatabase;
+	private final Database inner;
 
-	public TierCutDatabase(Configuration conf, long firstRecordIndex,
-			long numRecords, boolean reading, boolean writing)
-			throws IOException {
+	public TierCutDatabase(String uri, Configuration conf,
+			long firstRecordIndex, long numRecords, boolean reading,
+			boolean writing) throws IOException {
 		super(conf, firstRecordIndex, numRecords, reading, writing);
+		this.uri = uri;
 		myTierGame = (TierGame) conf.getGame();
-		deleteLastRow = true;
-		numTiersCut = conf.getInteger("gamesman.database.tiers.cut", 2);
-		String dbClass = SplitLocalDatabase.class.getName();
-		String uri = conf.getProperty("gamesman.db.uri");
-		DataInputStream dis;
-		inner = (SplitLocalDatabase) Database.openDatabase(dbClass, uri, conf,
-				firstRecordIndex, numRecords, reading, writing);
+		int numberOfTiers = myTierGame.numberOfTiers();
+		int[] preservedTiers = Util.parseIntArray(conf.getProperty(
+				"gamesman.database.stored.tiers", null));
+		if (preservedTiers == null)
+			preservedTiers = makeArray(
+					conf.getInteger("gamesman.database.tiers.cut", 2),
+					numberOfTiers);
+		inDatabase = new boolean[numberOfTiers];
+		for (int i : preservedTiers) {
+			inDatabase[i] = true;
+		}
+		String innerUri = conf.getProperty("gamesman.db.inner.uri");
+		try {
+			inner = Database.openDatabase(innerUri);
+		} catch (ClassNotFoundException e) {
+			throw new Error(e);
+		}
+	}
+
+	public TierCutDatabase(Database inner, String uri, Configuration conf,
+			long firstRecordIndex, long numRecords, int[] preservedTiers)
+			throws IOException {
+		super(conf, firstRecordIndex, numRecords, false, true);
+		this.uri = uri;
+		myTierGame = (TierGame) conf.getGame();
+		inDatabase = new boolean[myTierGame.numberOfTiers()];
+		for (int i : preservedTiers) {
+			inDatabase[i] = true;
+		}
+		this.inner = inner;
+	}
+
+	private int[] makeArray(int numTiersCut, int numberOfTiers) {
+		int[] preserved = new int[(numberOfTiers + numTiersCut)
+				/ (numTiersCut + 1)];
+		for (int i = 0; i < preserved.length; i++) {
+			preserved[i] = i * (numTiersCut + 1);
+		}
+		return preserved;
 	}
 
 	@Override
@@ -36,14 +73,14 @@ public final class TierCutDatabase extends Database {
 			throws IOException {
 		int tier = myTierGame.hashToTier(recordIndex);
 		if (shouldBeInDatabase(tier)) {
-			//return super.readRecord(dh, recordIndex);
+			// return super.readRecord(dh, recordIndex);
 			return inner.readRecord(dh, recordIndex);
 		} else {
-			if (inLastRow(recordIndex)) {
-				TierState pos = myTierGame.hashToState(recordIndex);
-				myTierGame.setState(pos);
-				Record myRecord = myTierGame.newRecord();
-				myRecord.value = myTierGame.primitiveValue();
+			TierState pos = myTierGame.hashToState(recordIndex);
+			myTierGame.setState(pos);
+			Record myRecord = myTierGame.newRecord();
+			myRecord.value = myTierGame.primitiveValue();
+			if (myRecord.value != Value.UNDECIDED) {
 				myRecord.remoteness = 0;
 				long val = myTierGame.recordToLong(myRecord);
 				return val;
@@ -79,26 +116,13 @@ public final class TierCutDatabase extends Database {
 			throws IOException {
 		int tier = myTierGame.hashToTier(recordIndex);
 		if (shouldBeInDatabase(tier)) {
-			//super.writeRecord(dh, recordIndex, r);
+			// super.writeRecord(dh, recordIndex, r);
 			inner.writeRecord(dh, recordIndex, r);
 		}
 	}
 
 	private final boolean shouldBeInDatabase(int tier) {
-		// Unsure if this is right, only for last row
-		if (tier == myTierGame.numberOfTiers() - 1) {
-			return false;
-		} else {
-			return tier % (numTiersCut + 1) == 0;
-		}
-
-	}
-
-	private boolean inLastRow(long curByte) {
-		return deleteLastRow
-				&& curByte < (myTierGame.numHashes() - myTierGame
-						.numHashesForTier(myTierGame.numberOfTiers() - 1));
-
+		return inDatabase[tier];
 	}
 
 	@Override
@@ -115,46 +139,60 @@ public final class TierCutDatabase extends Database {
 
 	@Override
 	public DatabaseHandle getHandle(boolean reading) {
-		return new TierCutDatabaseHandle(myLogic.recordBytes, reading);
-	}
-
-	private class TierCutDatabaseHandle extends DatabaseHandle {
-		private final DatabaseHandle innerHandle;
-
-		public TierCutDatabaseHandle(int numBytes, boolean reading) {
-			super(numBytes, reading);
-			innerHandle = inner.getHandle(reading);
-		}
+		return inner.getHandle(reading);
 	}
 
 	@Override
 	protected void lowerPrepareReadRange(DatabaseHandle dh,
 			long firstByteIndex, long numBytes) throws IOException {
-		inner.lowerPrepareReadRange(((TierCutDatabaseHandle) dh).innerHandle,
-				firstByteIndex, numBytes);
+		inner.lowerPrepareReadRange(dh, firstByteIndex, numBytes);
 	}
-	
+
 	@Override
 	protected void lowerPrepareWriteRange(DatabaseHandle dh,
 			long firstByteIndex, long numBytes) throws IOException {
-		inner.lowerPrepareWriteRange(((TierCutDatabaseHandle) dh).innerHandle, firstByteIndex, numBytes);
+		inner.lowerPrepareWriteRange(dh, firstByteIndex, numBytes);
 
 	}
-	
+
 	@Override
 	protected int lowerReadBytes(DatabaseHandle dh, byte[] array, int off,
 			int maxLen) throws IOException {
-		return inner.lowerReadBytes(((TierCutDatabaseHandle) dh).innerHandle, array, off, maxLen);
+		return inner.lowerReadBytes(dh, array, off, maxLen);
 	}
-	
+
 	@Override
 	protected int lowerWriteBytes(DatabaseHandle dh, byte[] array, int off,
 			int maxLen) throws IOException {
-		return inner.lowerWriteBytes(((TierCutDatabaseHandle) dh).innerHandle, array, off, maxLen);
+		return inner.lowerWriteBytes(dh, array, off, maxLen);
 	}
-	
+
 	@Override
 	public void close() throws IOException {
+		if (writing) {
+			DataOutputStream dos = new DataOutputStream(new FileOutputStream(
+					uri));
+			writeHeader(dos);
+			dos.close();
+		}
 		inner.close();
+	}
+
+	public static void main(String[] args) throws IOException,
+			ClassNotFoundException {
+		Database inner = Database.openDatabase(args[0]);
+		Configuration conf = inner.conf;
+		int[] storedTiers = new int[args.length - 2];
+		for (int i = 0; i < storedTiers.length; i++) {
+			storedTiers[i] = Integer.parseInt(args[i + 2]);
+		}
+		Arrays.sort(storedTiers);
+		conf.setProperty("gamesman.database.stored.tiers",
+				Arrays.toString(storedTiers));
+		conf.setProperty("gamesman.db.inner.uri", args[0]);
+		conf.setProperty("gamesman.database", TierCutDatabase.class.getName());
+		TierCutDatabase newDb = new TierCutDatabase(inner, args[1], conf,
+				inner.firstRecordIndex, inner.numRecords, storedTiers);
+		newDb.close();
 	}
 }
