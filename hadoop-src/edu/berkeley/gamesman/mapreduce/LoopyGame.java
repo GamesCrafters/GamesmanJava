d9ea96a79b9dff8869fbd8fd54a4fae1a1a7b426
dspyz <dspyz@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2011-04-23 19:18:03 +0000

    No longer needed
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@6206 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- hadoop-src/edu/berkeley/gamesman/mapreduce/LoopyGame.java
+++
@@ -1,8 +0,0 @@
-package edu.berkeley.gamesman.mapreduce;
-
-public interface LoopyGame {
-	public Iterable<Long> getSuccessors(long hash);
-	public boolean isPrimitive(long hash);
-	public int evalPrimitive(long hash);
-	public Iterable<Long> getStartingPositions();
-}
