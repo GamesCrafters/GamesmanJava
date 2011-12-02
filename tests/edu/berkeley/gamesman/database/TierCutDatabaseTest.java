82e99c5484a812cd3c6099cdf43c49aced9ec953
rchengyue <rchengyue@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2011-12-02 14:09:14 +0000

    Created a testing folder and TierCutDatabaseTest.
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@6480 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ tests/edu/berkeley/gamesman/database/TierCutDatabaseTest.java
@@ -0,0 +1,14 @@
+package edu.berkeley.gamesman.database;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TierCutDatabaseTest {
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
