fa949c1064d013a2699f0453fa24996d5ec21d2a
dspyz <dspyz@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2012-01-08 02:48:16 +0000

    Started writing BaghChal
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@6522 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ GamesmanJava-parallel/src/main/java/edu/berkeley/gamesman/hadoop/game/baghchal/BaghChalState.java
@@ -0,0 +1,51 @@
+package edu.berkeley.gamesman.hadoop.game.baghchal;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import edu.berkeley.gamesman.propogater.writable.WritableSettableComparable;
+
+public class BaghChalState<T extends BaghChalState<T>> implements
+		WritableSettableComparable<T> {
+
+	public BaghChalState(int width, int height, int tigers, int goats) {
+
+	}
+
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void write(DataOutput out) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void set(T t) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int compareTo(T o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
