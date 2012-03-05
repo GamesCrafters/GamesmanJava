a141de38f7aa6e792475fba37ecad03e01e6b092
willshen <willshen@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2012-03-05 19:54:34 +0000

    initial commit with the sketch of the state
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@6676 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ GamesmanJava-parallel/src/main/java/edu/berkeley/gamesman/parallel/game/tootandotto/TOState.java
@@ -0,0 +1,15 @@
+package edu.berkeley.gamesman.parallel.game.tootandotto;
+
+/**
+ * Similar to the C4State for connect 4, it holds the game as a list of pieces
+ * in col-major order. The highest index in the array is the number of pieces on
+ * the board. The second and third highest, respectively, are the number of Ts
+ * and Os placed by player 1. The fourth and fifth are the number of Ts and Os
+ * placed by player 2. The length of the array is gameSize + 5.
+ * 
+ * @author williamshen
+ * 
+ */
+public class TOState {
+
+}
