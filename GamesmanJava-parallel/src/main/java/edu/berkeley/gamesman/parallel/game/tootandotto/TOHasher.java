63d5fb27e414ae875575cbf37fbee3de2f872e42
willshen <willshen@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2012-03-08 07:14:20 +0000

    initial check in, filled in the constructor
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@6697 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ GamesmanJava-parallel/src/main/java/edu/berkeley/gamesman/parallel/game/tootandotto/TOHasher.java
@@ -0,0 +1,63 @@
+package edu.berkeley.gamesman.parallel.game.tootandotto;
+
+import java.util.Arrays;
+
+import edu.berkeley.gamesman.hasher.genhasher.DBInvCalculator;
+import edu.berkeley.gamesman.hasher.invhasher.OptimizingInvariantHasher;
+import edu.berkeley.gamesman.parallel.game.connect4.C4State;
+
+public class TOHasher extends OptimizingInvariantHasher<TOState> {
+	private final int width, height;
+	public final int boardSize; // TODO: why not private?
+	private final DBInvCalculator calc;
+
+	public TOHasher(int width, int height) {
+		this(width, height, 0);
+	}
+
+	/**
+	 * @param width
+	 *            The width of the board
+	 * @param height
+	 *            The height of the board
+	 * @param countingPlace
+	 *            The size of the ranges which we wish to count. <-- Should only
+	 *            be used for evaluating the average number of states per range
+	 *            per state
+	 */
+	public TOHasher(int width, int height, int countingPlace) {
+		super(makeDigitBase(width, height), countingPlace);
+		this.width = width;
+		this.height = height;
+		boardSize = width * height;
+		calc = new DBInvCalculator(boardSize);
+	}
+
+	private static int[] makeDigitBase(int width, int height) {
+		int boardSize = width * height;
+		int[] digitBase = new int[boardSize + 5];
+		Arrays.fill(digitBase, 3);
+		Arrays.fill(digitBase, boardSize - 4, boardSize - 1, 9);
+		digitBase[boardSize] = boardSize + 1;
+		return digitBase;
+	}
+
+	@Override
+	protected long getInvariant(TOState state) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected boolean valid(TOState state) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected TOState genHasherNewState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
