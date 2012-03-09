adb2d98e0d8b456932383f9c7b6513387c99af4e
bryanchu <bryanchu@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2012-03-09 21:15:50 +0000

    test commit
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@6721 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- GamesmanJava-parallel/src/main/java/edu/berkeley/gamesman/parallel/game/connections/Connections.java
+++ GamesmanJava-parallel/src/main/java/edu/berkeley/gamesman/parallel/game/connections/Connections.java
@@ -1,8 +1,13 @@
 package edu.berkeley.gamesman.parallel.game.connections;
 
+
+
 import java.util.Collection;
+import java.util.Collections;
 
 import edu.berkeley.gamesman.game.type.GameValue;
+import edu.berkeley.gamesman.hasher.DBHasher;
+import edu.berkeley.gamesman.hasher.counting.CountingState;
 import edu.berkeley.gamesman.hasher.genhasher.GenHasher;
 import edu.berkeley.gamesman.hasher.genhasher.Move;
 import edu.berkeley.gamesman.parallel.game.connect4.C4Hasher;
@@ -10,17 +15,17 @@ import edu.berkeley.gamesman.parallel.ranges.RangeTree;
 import edu.berkeley.gamesman.solve.reader.SolveReader;
 import edu.berkeley.gamesman.util.Pair;
 
-public class Connections extends RangeTree<ConnectionsState> implements
-SolveReader<ConnectionsState>{
+public class Connections extends RangeTree<CountingState> implements
+SolveReader<CountingState>{
 	
 	private Move[] myMoves;
 	private Move[][] colMoves;
-	private C4Hasher myHasher;
+	private DBHasher myHasher;
 	private int width, height, inARow;
 	private int gameSize;
 	private int suffLen;
 	
-	public String getString(ConnectionsState position) {
+	public String getString(CountingState position) {
 		StringBuilder sb = new StringBuilder(gameSize);
 		for (int row = 0; row < height; row++) {
 			for (int col = 0; col < width; col++) {
@@ -36,32 +41,32 @@ SolveReader<ConnectionsState>{
 	}
 
 	@Override
-	public ConnectionsState getPosition(String board) {
+	public CountingState getPosition(String board) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Collection<Pair<String, ConnectionsState>> getChildren(
-			ConnectionsState position) {
+	public Collection<Pair<String, CountingState>> getChildren(
+			CountingState position) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public GameValue getValue(ConnectionsState state) {
+	public GameValue getValue(CountingState state) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Collection<ConnectionsState> getStartingPositions() {
-		// TODO Auto-generated method stub
-		return null;
+	public Collection<CountingState> getStartingPositions() {
+		CountingState result = myHasher.newState(); //newState() calls the CountingState constructor
+		return Collections.singleton(result);
 	}
 
 	@Override
-	public GenHasher<ConnectionsState> getHasher() {
+	public GenHasher<CountingState> getHasher() {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -77,4 +82,11 @@ SolveReader<ConnectionsState>{
 		// TODO Auto-generated method stub
 		return 0;
 	}
+	
+
+
+
+
+
+
 }
