f2c181941fd00c3a531c6c57368ade95b8dda6bf
enigmaplatypus <enigmaplatypus@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2012-03-16 18:59:54 +0000

    commented out bitset board
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@6757 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- GamesmanJava-parallel/src/main/java/edu/berkeley/gamesman/parallel/game/tootandotto/TOState.java
+++ GamesmanJava-parallel/src/main/java/edu/berkeley/gamesman/parallel/game/tootandotto/TOState.java
@@ -11,20 +11,53 @@ import edu.berkeley.gamesman.hasher.counting.CountingState;
  * placed by player 2. The length of the array is gameSize + 5.
  * 
  * @author williamshen
- * 
+ * need to allow players to be associated with a name, eg player one is toot, or otto choice
  */
 public class TOState extends CountingState {
 	private final int width, height, boardSize;
-	private final BitSetBoard myBoard; // TODO: need a different rep of the
-										// board
-
-	public TOState(TOHasher myHasher, int width, int height) {
+	private int player1T, player2T, player1O, player2O; //remaining T's and O's for the players
+	private int piecesOnBoard;
+	//private final BitSetBoard myBoard; // TODO: need a different rep of the
+									   // board
+	private int[][] boardRep;//col major order
+	/*
+	 * Initial state constructor
+	 * 
+	 */
+	public TOState(TOHasher myHasher, int width, int height) { 
 		super(myHasher, width * height);
 		this.width = width;
 		this.height = height;
 		this.boardSize = width * height;
-		this.myBoard = new BitSetBoard(height, width); // TODO: need a different
+		this.boardRep= new int[height][width];
+		
+		//this.myBoard = new BitSetBoard(height, width); // TODO: need a different
 														// rep of the board
+		for(int i=0; i<height; i++)
+			for(int j=0; j<width; j++)
+				this.boardRep[i][j]=0;
+		
+		
 	}
-
+	/*
+	 * Copies board from parent
+	 */
+	public TOState(TOHasher myHasher, int width, int height, TOState parentState) { 
+		super(myHasher, width * height);
+		this.width = width;
+		this.height = height;
+		this.boardSize = width * height;
+		this.boardRep= new int[height][width];
+		
+		//this.myBoard = new BitSetBoard(height, width); // TODO: need a different
+														// rep of the board
+		//could probably do clone on parent...
+		for(int i=0; i<height; i++)
+			for(int j=0; j<width; j++)
+				this.boardRep[i][j]=parentState.boardRep[i][j];
+		
+		
+	}
 }
+
+
