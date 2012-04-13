dc0423b0fbd2ab0f6475ceecafe5427b040fd064
willshen <willshen@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2012-04-13 18:47:05 +0000

    enum to rid magic numbers
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanJava@6805 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

---
+++ GamesmanJava-parallel/src/main/java/edu/berkeley/gamesman/parallel/game/tootandotto/TOEnum.java
@@ -0,0 +1,10 @@
+package edu.berkeley.gamesman.parallel.game.tootandotto;
+
+/**
+ * ENUMs to be used for TOOT-N-OTTO 
+ * @author williamshen
+ *
+ */
+public enum TOEnum {
+	PLAYER1, PLAYER2, DETECT_TOOT, DETECT_OTTO, DETECT_BOTH, DETECT_NONE
+}
